57830c8d60730287f4ab437dc90bdbdc429ec5ff
Roman Isopenko <isopenkoroman@gmail.com>
2023-06-18 14:19:15 +0200

    Fix small bug of Router, create Register page component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
         <Routes>
-            <Route path="/login" element={Login}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
         </Routes>
-      <Login />
     </Router>
   );
 }
