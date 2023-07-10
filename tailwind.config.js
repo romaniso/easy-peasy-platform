c19b74f5f974388211d89990a30d15d05cbf030d
Roman Isopenko <isopenkoroman@gmail.com>
2023-07-10 21:51:00 +0200

    Add dropdowns for Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    // colors: {
-    //   primary: colors.indigo,
-    //   secondary: colors.yellow,
-    // }
-  },
+  //  theme: {
+  //     colors: {
+  //       primary: colors.indigo,
+  //       secondary: colors.yellow,
+  //     }
+  //  },
   plugins: [],
 };
