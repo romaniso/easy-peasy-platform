b2b3b035651c40820035a93f3fcd4686506151c2
Roman Isopenko <isopenkoroman@gmail.com>
2023-09-17 21:51:09 +0200

    Add condition for muting Navbar on dashboard page using useLocation react-router hook

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 function Layout({ children }) {
+  let { pathname } = useLocation();
   return (
     <div className="bg-gradient-to-r from-violet-50 via-fuchsia-100 to-indigo-200 min-h-full overflow-hidden flex flex-col">
-      <Navbar />
+      {pathname !== "/dashboard" && <Navbar />}
       <main className="grow shrink basis-auto">{children}</main>
       <Footer />
     </div>
