216ebfb18247accb58611b3120d377ba52616317
Roman Isopenko <isopenkoroman@gmail.com>
2023-10-01 21:38:22 +0200

    Start creating Exercise component

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,6 +1,85 @@
 import React from "react";
+import Panel from "./Panel";
 
-function Exercise() {
-  return <section>Exercise</section>;
+function Exercise({
+  instruction = "Choose the correct or most appropriate future forms to complete the sentences below.",
+  title = "Will / be going to / present continuous for future",
+  questions = [
+    {
+      question: "I *** visit my grandmother tomorrow.",
+      options: [
+        { text: "will", isCorrect: false },
+        { text: "am going to", isCorrect: true },
+        { text: "am visiting", isCorrect: false },
+      ],
+    },
+    {
+      question:
+        "They have tickets for the concert. They *** attend it tonight.",
+      options: [
+        { text: "will", isCorrect: false },
+        { text: "are going to", isCorrect: true },
+        { text: "are attending", isCorrect: false },
+      ],
+    },
+    {
+      question: "I think it *** rain later, so don't forget your umbrella.",
+      options: [
+        { text: "will", isCorrect: false },
+        { text: "is going to", isCorrect: true },
+        { text: "is raining", isCorrect: false },
+      ],
+    },
+    {
+      question: "She *** fly to Paris next week for a business meeting.",
+      options: [
+        { text: "will", isCorrect: false },
+        { text: "is going to", isCorrect: true },
+        { text: "is flying", isCorrect: false },
+      ],
+    },
+    {
+      question:
+        "We *** have a picnic at the park on Saturday if the weather is nice.",
+      options: [
+        { text: "will", isCorrect: false },
+        { text: "are going to", isCorrect: true },
+        { text: "are having", isCorrect: false },
+      ],
+    },
+  ],
+}) {
+  //  const renderQuestion = (question, select) => {
+  //    return question.replace("***", "_____");
+  //  };
+
+  const renderedExercise = questions.map(({ question, options }, index) => {
+    const renderedOptions = options.map((option, index) => {
+      return (
+        <option data-correct={option.isCorrect} key={index}>
+          {option.text}
+        </option>
+      );
+    });
+
+    const renderedQuestion = question.replace("***", renderedOptions);
+
+    return <li key={index}>{renderedQuestion}</li>;
+  });
+
+  return (
+    <Panel className="bg-white px-12 py-10">
+      <h2 className="text-3xl font-bold text-indigo-800 mb-8">{title}</h2>
+      <p className="text-2xl font-bold text-indigo-400 mb-4">
+        Task Description
+      </p>
+      <p className="text-base text-indigo-300 bg-stone-700 shadow p-5 mb-4 rounded-lg">
+        {instruction}
+      </p>
+      <section>
+        <ul>{renderedExercise}</ul>
+      </section>
+    </Panel>
+  );
 }
 export default Exercise;
--- src/pages/ExercisePage.js
+++ src/pages/ExercisePage.js
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom";
+import Exercise from "../components/Exercise";
+//TODO Here I will probably implement API request for data base where I get all content for exercises, instructions, etc based on path of URL and then send it to the Exercise component. Maybe I will create ExerciseSet component as well
 
 function ExercisePage() {
   const { topic } = useParams();
@@ -8,6 +10,7 @@ function ExercisePage() {
       <h1 className="text-6xl text-center font-bold text-orange-500 drop-shadow mb-8">
         {topic}
       </h1>
+      <Exercise />
     </div>
   );
 }
