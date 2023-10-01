e47dd4109fc21bc0ac8c01891189c7d6c257e4d4
Roman Isopenko <isopenkoroman@gmail.com>
2023-10-01 19:15:30 +0200

    Fix footer stick to bottom issue

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,9 +5,9 @@ import Footer from "./Footer";
 function Layout({ children }) {
   let { pathname } = useLocation();
   return (
-    <div className="bg-gradient-to-r from-violet-50 via-fuchsia-100 to-indigo-200 min-h-full overflow-hidden flex flex-col">
+    <div className="bg-gradient-to-r from-violet-50 via-fuchsia-100 to-indigo-200 overflow-hidden flex flex-col min-h-screen">
       {pathname !== "/dashboard" && <Navbar />}
-      <main className="grow shrink basis-auto">{children}</main>
+      <main className="flex-1">{children}</main>
       {pathname !== "/dashboard" && <Footer />}
     </div>
   );
