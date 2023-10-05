9c95059decd060b1890084841de8cd0e8b5d4246
Roman Isopenko <isopenkoroman@gmail.com>
2023-10-05 11:17:16 +0200

    Refactor Exercise component

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -53,69 +53,16 @@ function Exercise({
     },
   ],
 }) {
-  // Initialize state to store selected values
   const [selectedValues, setSelectedValues] = useState(
     Array(questions.length).fill("")
-  ); // Initialize with empty values
+  );
   const [userResults, setUserResults] = useState(null);
 
   useEffect(() => {
-    // This code will run whenever userResults changes
+    // if (userResults !== null) {
+    //   console.log(showFeedback(userResults));
+    // }
   }, [userResults]);
-
-  const renderedExercise = questions.map(({ question, options }, index) => {
-    //FIXME: too complex function. Try to break it dowm a bit. Separate feedback icons from questions, selects
-    let feedbackIcon = null;
-    if (userResults) {
-      if (userResults[index] === "Same")
-        feedbackIcon = (
-          <FaRegThumbsUp className="inline-block text-green-500 ml-2 500 500" />
-        );
-      else if (userResults[index] === "Different")
-        feedbackIcon = (
-          <FaRegThumbsDown className="inline-block ml-2  text-red-400" />
-        );
-    }
-
-    const renderedQuestion = question.split("***").map((part, partIndex) => {
-      if (partIndex === 1) {
-        //FIXME: try to replace *** with something generic, not hard-coded. Stll, I have to mark the place of splitting somehow in a string
-        // Replace *** with the rendered options
-        return (
-          <>
-            <select
-              className="text-xl p-1 border rounded-md shadow-inner text-indigo-800 cursor-pointer outline-none"
-              key={index}
-              onChange={(e) => handleSelectChange(index, e)}
-              value={selectedValues[index] || undefined} // Bind the value to the selected value in state
-            >
-              <option disabled selected value></option>
-              {options.map((option, optionIndex) => (
-                <option
-                  className="hover:bg-orange-200"
-                  data-correct={option.isCorrect}
-                  value={option.text}
-                  key={optionIndex}
-                >
-                  {option.text}
-                </option>
-              ))}
-            </select>
-            {part}
-            {feedbackIcon}
-          </>
-        );
-      } else {
-        // Keep the original text part
-        return <span key={index}>{part}</span>;
-      }
-    });
-    return (
-      <li className="text-indigo-900 text-xl mb-10 " key={index}>
-        {renderedQuestion}
-      </li>
-    );
-  });
 
   const handleSelectChange = (index, event) => {
     const updatedValues = [...selectedValues];
@@ -124,62 +71,89 @@ function Exercise({
   };
 
   const showFeedback = (results) => {
-    if (results) {
-      let correctAnswers = 0;
-      const questionsNumber = questions.length;
+    if (!results) return null;
 
-      for (const answer of results) {
-        if (answer === "Same") {
-          correctAnswers++;
-        }
-      }
+    const correctAnswers = results.filter((answer) => answer === "Same").length;
+    const questionsNumber = questions.length;
 
-      return (
-        <p className="text-xl bg-stone-50 shadow-inner p-5 mb-4 rounded-lg inline-block">
-          Your score:{" "}
-          <span
-            className={
-              correctAnswers
-                ? "text-green-500 font-bold"
-                : "text-red-500 font-bold"
-            }
-          >
-            {correctAnswers}
-          </span>
-          /<span className="text-indigo-500 font-bold">{questionsNumber}</span>
-        </p>
-      );
-    }
+    return (
+      <p className="text-xl text-indigo-900 bg-stone-50 shadow-inner p-5 mb-4 rounded-lg inline-block">
+        Your score:{" "}
+        <span
+          className={
+            correctAnswers
+              ? "text-green-500 font-bold"
+              : "text-red-500 font-bold"
+          }
+        >
+          {correctAnswers}
+        </span>
+        /<span className="text-indigo-500 font-bold">{questionsNumber}</span>
+      </p>
+    );
   };
 
   const validateUsersAnswers = (usersAnswers) => {
-    //  Create a keysheet for thw following validation out of props object
     const keySheet = questions.map((question) => {
-      for (const option of question.options) {
-        if (option.isCorrect) return option.text;
-      }
+      return question.options.find((option) => option.isCorrect).text;
     });
 
-    const result = [];
+    const result = usersAnswers.map((answer, index) => {
+      return answer === keySheet[index] ? "Same" : "Different";
+    });
 
-    // Compare user's answers with keysheet
-    for (let i = 0; i < Math.max(keySheet.length, usersAnswers.length); i++) {
-      if (keySheet[i] === usersAnswers[i]) {
-        result.push("Same");
-      } else {
-        result.push("Different");
-      }
-    }
-
-    //Show feedback
     setUserResults(result);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Perform your validation logic here
     validateUsersAnswers(selectedValues);
   };
+
+  const renderedExercise = questions.map(({ question, options }, index) => {
+    const feedbackIcon =
+      userResults &&
+      (userResults[index] === "Same" ? (
+        <FaRegThumbsUp className="inline-block text-green-500 ml-2" />
+      ) : (
+        <FaRegThumbsDown className="inline-block ml-2 text-red-400" />
+      ));
+
+    const renderedQuestion = (
+      <li className="text-indigo-900 text-xl mb-10" key={index}>
+        {question.split("***").map((part, partIndex) => {
+          return partIndex === 1 ? (
+            <>
+              <select
+                className="text-xl p-1 border rounded-md shadow-inner text-indigo-800 cursor-pointer outline-none"
+                key={index}
+                onChange={(e) => handleSelectChange(index, e)}
+                value={selectedValues[index] || ""}
+              >
+                <option disabled value=""></option>
+                {options.map((option, optionIndex) => (
+                  <option
+                    className="hover:bg-orange-200"
+                    data-correct={option.isCorrect}
+                    value={option.text}
+                    key={optionIndex}
+                  >
+                    {option.text}
+                  </option>
+                ))}
+              </select>
+              {part}
+              {feedbackIcon}
+            </>
+          ) : (
+            <span key={index}>{part}</span>
+          );
+        })}
+      </li>
+    );
+
+    return renderedQuestion;
+  });
 
   return (
     <Panel className="bg-white px-12 py-10">
@@ -190,7 +164,7 @@ function Exercise({
       <p className="text-base text-orange-500 bg-stone-50 shadow-inner p-5 mb-4 rounded-lg">
         {instruction}
       </p>
-      {userResults && <h3>{showFeedback(userResults)}</h3>}
+      {userResults && showFeedback(userResults)}
       <form onSubmit={handleSubmit}>
         <ul>{renderedExercise}</ul>
         <Button primary rounded className="w-1/5" type="submit">
