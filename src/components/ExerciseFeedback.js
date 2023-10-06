df01b8489ae21ea62fa4d0186150db8db7f7dd73
Roman Isopenko <isopenkoroman@gmail.com>
2023-10-06 10:25:36 +0200

    Refactor ExerciseFeedback component

--- src/components/ExerciseFeedback.js
+++ src/components/ExerciseFeedback.js
@@ -1,20 +1,19 @@
 function ExerciseFeedback({ results, questionsNumber }) {
-  if (!results) return null;
-
-  const correctAnswers = results.filter((answer) => answer === "Same").length;
-  return (
+  return results ? (
     <p className="text-xl text-indigo-900 bg-stone-50 shadow-inner p-5 mb-4 rounded-lg inline-block">
       Your score:{" "}
       <span
         className={
-          correctAnswers ? "text-green-500 font-bold" : "text-red-500 font-bold"
+          results.filter((answer) => answer === "Same").length
+            ? "text-green-500 font-bold"
+            : "text-red-500 font-bold"
         }
       >
-        {correctAnswers}
+        {results.filter((answer) => answer === "Same").length}
       </span>
       /<span className="text-indigo-500 font-bold">{questionsNumber}</span>
     </p>
-  );
+  ) : null;
 }
 
 export default ExerciseFeedback;
