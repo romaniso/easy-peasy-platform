81dec064574386bc1c444eda9bcc01627af0ecde
Roman Isopenko <isopenkoroman@gmail.com>
2023-10-19 21:09:16 +0200

    Create ExerciseFill component

--- src/components/ExerciseBody.js
+++ src/components/ExerciseBody.js
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import ExerciseDraggable from "./ExerciseDraggable";
 import ExerciseDropdown from "./ExerciseDropdown";
+import ExerciseFill from "./ExerciseFill";
 import { FaRegThumbsUp, FaRegThumbsDown } from "react-icons/fa";
 
 function ExerciseBody({
@@ -36,47 +37,11 @@ function ExerciseBody({
       break;
     case "fill-in":
       renderedExercise = (
-        <ul>
-          {questions.map((item, index) => {
-            const feedbackIcon =
-              results &&
-              (results[index] === "Same" ? (
-                <FaRegThumbsUp className="inline-block text-green-500 ml-2" />
-              ) : (
-                <FaRegThumbsDown className="inline-block ml-2 text-red-400" />
-              ));
-
-            const { question } = item;
-            const renderedQuestion = (
-              <li className="text-indigo-900 text-xl mb-8" key={index}>
-                {question.split("***").map((part, partIndex) => {
-                  //Conditional Render
-                  const inputPart = (
-                    <>
-                      <input
-                        className="text-xl p-1 border rounded-md shadow-inner text-indigo-800 outline-none"
-                        key={index}
-                        name="selection"
-                        type="text"
-                        autoComplete="off"
-                        onChange={(e) => handleSelectChange(index, e)}
-                      />
-                      {part}
-                      {feedbackIcon}
-                    </>
-                  );
-                  return partIndex === 1 ? (
-                    inputPart
-                  ) : (
-                    <span key={index}>{part}</span>
-                  );
-                })}
-              </li>
-            );
-
-            return renderedQuestion;
-          })}
-        </ul>
+        <ExerciseFill
+          questions={questions}
+          results={results}
+          onChange={handleSelectChange}
+        />
       );
       break;
     case "drag-&-drop":
