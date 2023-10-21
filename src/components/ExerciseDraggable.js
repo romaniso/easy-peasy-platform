f5c7cc7fb7eba6f041a0c88b79de5204effadf27
Roman Isopenko <isopenkoroman@gmail.com>
2023-10-21 20:20:42 +0200

    Implement shuffle logics for Draggable

--- src/components/ExerciseDraggable.js
+++ src/components/ExerciseDraggable.js
@@ -7,30 +7,36 @@ import { DndContext } from "@dnd-kit/core";
 //TODO: This component definitely requires a lot of refactoring. A user can currently dnd in one way but cannot drag a draggable component backwards.
 
 function ExerciseDraggable({ draggables, droppables, onSelect, results }) {
-  //  const [shuffledDrags, setShuffledDrags] = useState([]);
-  const [toDrags, setDraggables] = useState(draggables);
+  const [shuffledDrags, setShuffledDrags] = useState([]);
+  //  const [toDrags, setDraggables] = useState(draggables);
   const [toDrops, setDroppables] = useState(droppables);
 
   //FIXME: I need to shuffle the answers
-  //  function shuffleArray(array) {
-  //    for (let i = array.length - 1; i > 0; i--) {
-  //      const j = Math.floor(Math.random() * (i + 1));
-  //      [array[i], array[j]] = [array[j], array[i]];
-  //    }
-  //  }
-  // Initialize the shuffledDrags when the component mounts
-  //  useEffect(() => {
-  //    const initialShuffledDrags = [...draggables]; // Create a copy of the draggable items
-  //    shuffleArray(initialShuffledDrags); // Shuffle the copy
-  //    setShuffledDrags(initialShuffledDrags); // Set the shuffled items as the initial state
-  //  }, [draggables]); // Trigger the effect when draggable items change
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  //Initialize the shuffledDrags when the component mounts
+  useEffect(() => {
+    const initialShuffledDrags = [...draggables]; // Create a copy of the draggable items
+    shuffleArray(initialShuffledDrags); // Shuffle the copy
+    setShuffledDrags(
+      initialShuffledDrags.map((drag, index) => {
+        return { ...drag, id: index };
+      })
+    ); // Set the shuffled items as the initial state
+  }, []); // Trigger the effect when draggable items change
 
   function handleDragEnd(event) {
     if (event.over && event.over.data.current.type === "droppable") {
       const droppableId = Number(event.over.id);
       const draggableId = Number(event.active.id);
 
-      const updatedToDrags = toDrags.map((drag) => {
+      console.log(droppableId, draggableId);
+
+      const updatedToDrags = shuffledDrags.map((drag) => {
         if (drag.id === draggableId && !toDrops[droppableId].isFilled) {
           return { ...drag, isPulled: true };
         }
@@ -39,14 +45,14 @@ function ExerciseDraggable({ draggables, droppables, onSelect, results }) {
 
       const updatedToDrops = toDrops.map((drop) => {
         if (drop.id === droppableId && !drop.isFilled) {
-          return { ...drop, isFilled: toDrags[draggableId].title };
+          return { ...drop, isFilled: shuffledDrags[draggableId].title };
         }
         return drop;
       });
 
-      setDraggables(updatedToDrags);
+      setShuffledDrags(updatedToDrags);
       setDroppables(updatedToDrops);
-      onSelect(droppableId, toDrags[draggableId].title);
+      onSelect(droppableId, shuffledDrags[draggableId].title);
     }
   }
 
@@ -79,9 +85,9 @@ function ExerciseDraggable({ draggables, droppables, onSelect, results }) {
     );
   });
   //Draggable
-  const drags = toDrags.map((drag) =>
+  const drags = shuffledDrags.map((drag, index) =>
     drag.isPulled ? null : (
-      <Draggable id={drag.id} title={drag.title} key={drag.id}></Draggable>
+      <Draggable id={drag.id} title={drag.title} key={index}></Draggable>
     )
   );
 
