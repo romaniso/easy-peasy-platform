8906dd874be6d4965c24c9f6cdc68d8bb90defb0
Roman Isopenko <isopenkoroman@gmail.com>
2023-10-29 16:55:49 +0100

    Add flip-card functionality for Flashcard component

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,23 +1,17 @@
 function Flashcard({ question, isCorrect, cardImage }) {
-    return (
-        <article className="group h-72 w-72 [perspective:1000px]">
-            <div className="relative h-full w-full rounded-md shadow-md shadow-black-600 overflow-hidden transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-                <div className="absolute inset-0 [backface-visibility: hidden]">
-                    <img
-                        src={cardImage}
-                        alt="card"
-                        className="h-full w-full object-cover"
-                    />
-                    <div className="absolute inset-0 text-center flex justify-center items-center bg-black/70">
-                        <p className="text-indigo-50 drop-shadow">{question}</p>
-                    </div>
-                </div>
-                <div className="absolute w-full h-full inset-0 bg-black [transform:rotateY(180deg)] [backface-visibility: hidden]">
-                    <p className="text-indigo-50 drop-shadow">{isCorrect}</p>
-                </div>
-            </div>
-        </article>
-    );
+  return (
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <img src={cardImage} alt="Avatar" />
+          <p>{isCorrect}</p>
+        </div>
+        <div className="flip-card-back">
+          <p>{question}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Flashcard;
