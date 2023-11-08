0a360a623f4b5f6c22b0adadf22fd3d2a2efe0ae
Roman Isopenko <isopenkoroman@gmail.com>
2023-11-08 20:07:20 +0100

    Fix input focus issue, add FillInLetterUnit

--- src/components/ExerciseFillInLetter.js
+++ src/components/ExerciseFillInLetter.js
@@ -1,10 +1,8 @@
 import {useEffect, useRef, useState} from "react";
+import FillInLetterUnit from "./FillInLetterUnit";
 
 function ExerciseFillInLetter({ questions, onChange, results }) {
     const [insertedWords, setInsertedWords] = useState([]);
-    const [activeInputIndexArr, setActiveInputIndexArr] = useState([0,0]);
-
-    const inputRef = useRef();
     const handleChange = ({target}, wordIndex, charIndex) => {
         //updating insertedWords array
         const {value} = target;
@@ -14,13 +12,7 @@ function ExerciseFillInLetter({ questions, onChange, results }) {
         updatedInsertedWords[wordIndex] = newWord;
         setInsertedWords(updatedInsertedWords);
 
-        // const updatedActiveInputIndexArr = [...activeInputIndexArr];
-        // while (insertedWords[wordIndex][charIndex + 1] !== '*'){
-        //     updatedActiveInputIndexArr[1]++;
-        // }
-        // setActiveInputIndexArr(activeInputIndexArr);
-        // if lastChar onChange up to selections
-
+        console.log(insertedWords[wordIndex]);
     }
 
     const randomizeIndex = (max) => Math.floor(Math.random() * max);
@@ -55,33 +47,34 @@ function ExerciseFillInLetter({ questions, onChange, results }) {
     }, []);
 
     const renderedExercise = insertedWords.map((word, wordIndex) => (
-        <div key={wordIndex}>
-      <span className="mr-2 md:mr-4 bg-indigo-400 text-white py-[2px] md:py-1 px-2 md:px-2 rounded-lg text-sm md:text-base shadow">
-        {wordIndex + 1}
-      </span>
-            <div className="inline-flex items-center gap-1 mb-6 md:mb-8">
-                {word.map((char, charIndex) => {
-                    if (word[charIndex] === "*") {
-                        return (
-                            <input
-                                //check if this is an active input
-                                ref={wordIndex === activeInputIndexArr[0] && charIndex === activeInputIndexArr[1] ? inputRef: null}
-                                key={charIndex}
-                                className="text-lg md:text-xl text-center md:p-1 border rounded-md shadow-inner text-indigo-800 font-bold outline-none w-6 md:w-8 hover:scale-105 focus:border-orange-300 hover:border-orange-300 transition-all duration-500"
-                                name="letter"
-                                type="text"
-                                autoComplete="off"
-                                onChange={(e) => handleChange(e, wordIndex, charIndex)}
-                            />
-                        );
-                    } else if (insertedWords[wordIndex][charIndex] === " ") {
-                        return <span key={charIndex} className="w-4 md:w-6">{char}</span>;
-                    } else {
-                        return <span key={charIndex} className="text-indigo-900 text-lg md:text-xl">{insertedWords[wordIndex][charIndex]}</span>;
-                    }
-                })}
-            </div>
-        </div>
+        <FillInLetterUnit word={word} wordIndex={wordIndex} onFill={handleChange} insertedWord={insertedWords[wordIndex]}/>
+      //   <div key={wordIndex}>
+      // <span className="mr-2 md:mr-4 bg-indigo-400 text-white py-[2px] md:py-1 px-2 md:px-2 rounded-lg text-sm md:text-base shadow">
+      //   {wordIndex + 1}
+      // </span>
+      //       <div className="inline-flex items-center gap-1 mb-6 md:mb-8">
+      //           {word.map((char, charIndex) => {
+      //               if (word[charIndex] === "*") {
+      //                   return (
+      //                       <input
+      //                           //check if this is an active input
+      //                           ref={wordIndex === activeInputIndexArr[0] && charIndex === activeInputIndexArr[1] ? inputRef: null}
+      //                           key={charIndex}
+      //                           className="text-lg md:text-xl text-center md:p-1 border rounded-md shadow-inner text-indigo-800 font-bold outline-none w-6 md:w-8 hover:scale-105 focus:border-orange-300 hover:border-orange-300 transition-all duration-500"
+      //                           name="letter"
+      //                           type="text"
+      //                           autoComplete="off"
+      //                           onChange={(e) => handleChange(e, wordIndex, charIndex)}
+      //                       />
+      //                   );
+      //               } else if (insertedWords[wordIndex][charIndex] === " ") {
+      //                   return <span key={charIndex} className="w-4 md:w-6">{char}</span>;
+      //               } else {
+      //                   return <span key={charIndex} className="text-indigo-900 text-lg md:text-xl">{insertedWords[wordIndex][charIndex]}</span>;
+      //               }
+      //           })}
+      //       </div>
+      //   </div>
     ));
 
     return <div>{renderedExercise}</div>;
---
+++ src/components/FillInLetterUnit.js
@@ -0,0 +1,57 @@
+import {useEffect, useRef, useState} from "react";
+function FillInLetterUnit({wordIndex, word, onFill, insertedWord}) {
+    const [activeInputIndex, setActiveInputIndex] = useState();
+
+    const inputRef = useRef();
+    const updateInput = (inputIndex) => {
+        if(inputIndex === word.length - 1) return;
+        let newIndex = inputIndex + 1;
+        if(word[newIndex] !== "*" || word[newIndex] === " "){
+            updateInput(newIndex);
+        } else {
+            console.log('Else')
+            setActiveInputIndex(newIndex);
+        }
+
+    }
+
+    useEffect(() => {
+        inputRef.current?.focus();
+
+    }, [activeInputIndex]);
+
+    return ( <div key={wordIndex}>
+      <span className="mr-2 md:mr-4 bg-indigo-400 text-white py-[2px] md:py-1 px-2 md:px-2 rounded-lg text-sm md:text-base shadow">
+        {wordIndex + 1}
+      </span>
+        <div className="inline-flex items-center gap-1 mb-6 md:mb-8">
+            {word.map((char, charIndex) => {
+                if (word[charIndex] === "*") {
+                    return (
+                        <input
+                            //check if this is an active input
+                            ref={charIndex === activeInputIndex ? inputRef: null}
+                            key={charIndex}
+                            className="text-lg md:text-xl text-center md:p-1 border rounded-md shadow-inner text-indigo-800 font-bold outline-none w-6 md:w-8 hover:scale-105 focus:border-orange-300 hover:border-orange-300 transition-all duration-500"
+                            name="letter"
+                            type="text"
+                            autoComplete="off"
+                            onChange={(e) => {
+                                onFill(e, wordIndex, charIndex)
+                                updateInput(charIndex)
+                            }}
+                            maxLength={1}
+                            value={insertedWord[charIndex] !== "*" ? insertedWord[charIndex] : undefined}
+                        />
+                    );
+                } else if (word[charIndex] === " ") {
+                    return <span key={charIndex} className="w-4 md:w-6">{char}</span>;
+                } else {
+                    return <span key={charIndex} className="text-indigo-900 text-lg md:text-xl">{word[charIndex]}</span>;
+                }
+            })}
+        </div>
+    </div>)
+}
+
+export default FillInLetterUnit;
