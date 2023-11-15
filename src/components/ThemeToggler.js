2319818808b344dab49bb451f5e7d198549301da
Roman Isopenko <isopenkoroman@gmail.com>
2023-11-15 12:24:34 +0100

    Style ThemeToggler

--- src/components/ThemeToggler.js
+++ src/components/ThemeToggler.js
@@ -1,4 +1,6 @@
 import {useEffect, useState} from "react";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+
 
 function ThemeToggler({className}){
     const [isDarkTheme, setIsDarkTheme] = useState(() => {
@@ -18,8 +20,10 @@ function ThemeToggler({className}){
         setIsDarkTheme(!isDarkTheme);
     }
 
-    return <button className={className} onClick={handleToggleTheme}>
-        Dark
+    const toggleButtonClasses = `border rounded-md w-10 h-10 p-3 border-stone-700 text-stone-700 hover:text-indigo-300 hover:bg-stone-700 transition-colors duration-700 ${className}`
+
+    return <button className={toggleButtonClasses} onClick={handleToggleTheme}>
+        {isDarkTheme ? <BsFillSunFill/> : <BsFillMoonStarsFill/>}
     </button>
 }
 
