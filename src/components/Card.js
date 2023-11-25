63bcd93028e41495ab303971c97bfc60e7fffae7
Roman Isopenko <isopenkoroman@gmail.com>
2023-11-25 18:02:52 +0100

    restyle Card, add badge prop

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 import Button from "./Button";
 import Panel from "./Panel";
 
-function Card({ title, text, image, buttonTxt, link }) {
+function Card({ title, text, image, buttonTxt, link, badge }) {
   return (
     <Panel className="bg-white shadow-lg flex flex-col md:max-w-xs transition-all duration-700 hover:scale-105">
       {image && (
-        <div className="h-28">
+        <div className="h-28 relative">
+            {badge && <div className='absolute text-2xl right-2 top-2 w-8 h-8 flex justify-center items-center rounded-md shadow-md bg-black/30 text-indigo-300 z-10'>{badge}</div>}
           <img
             src={image}
             alt="decor of the card"
@@ -14,9 +15,9 @@ function Card({ title, text, image, buttonTxt, link }) {
           />
         </div>
       )}
-      <div className="p-5 flex-col items-center gap-3 flex-auto">
+      <div className="py-5 flex-col items-center gap-3 flex-auto">
         <h3
-          className="font-semibold text-2xl overflow overflow-ellipsis whitespace-nowrap text-indigo-900 dark:text-orange-500 text-center"
+          className="font-semibold text-2xl overflow overflow-ellipsis text-indigo-900 dark:text-orange-500 text-center"
           title={title}
         >
           {title}
--- src/pages/PreviewPage.js
+++ src/pages/PreviewPage.js
@@ -17,13 +17,13 @@ function PreviewPage() {
             {/*It should be rendered dynamically fetching cards and images by API, not hardcoded. A loader should be included as well*/}
             <section className="flex justify-start flex-wrap items-stretch gap-12">
                 <Card
-                    title={"Relationships"}
-                    text={
-                        "People live among other people. Let's learn how to build relationships using the most common English phrases"
-                    }
+                    title="Building and Maintaining Relationships"
+                    text="People live among other people. Let's learn how to build relationships using the most common English phrases"
+
                     image={RelationshipsImg}
-                    buttonTxt={"Let's learn"}
-                    link={"/vocabulary/relationships"}
+                    buttonTxt="Let's learn"
+                    link="/reading/relationships"
+                    badge="A2"
                 />
             </section>
         </div>
