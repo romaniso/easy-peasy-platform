5e02bf7442d55709c9b514169320d5499c6e5035
Roman Isopenko <isopenkoroman@gmail.com>
2023-11-29 21:38:42 +0100

    config mongodb

--- server/app.js
+++ server/app.js
@@ -1,1 +1,11 @@
-console.log('Hello from backend')
+const {section} = require("./config/db");
+console.log('Hello from backend');
+
+(async () => {
+    const sections = await section.find();
+    console.log(await sections.toArray());
+})();
+
+
+
+
---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+const {MongoClient} = require('mongodb');
+
+const client = new MongoClient('mongodb://127.0.0.1:27017/');
+
+const db = client.db('easy-peasy-db');
+const section = db.collection('section');
+const exerciseSet = db.collection('exerciseSet');
+const exercise = db.collection('exercise');
+const textual = db.collection('textual');
+
+module.exports = {
+    db,
+    client,
+    section,
+    exerciseSet,
+    exercise,
+    textual,
+};
