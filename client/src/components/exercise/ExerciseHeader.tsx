aa8e06ee982b4f03694eaa64df1a8f5438a34dd4
Roman Isopenko <isopenkoroman@gmail.com>
2023-12-08 21:43:45 +0100

    refactor: migrate ExerciseHeader to TypeScript

---
+++ client/src/components/exercise/ExerciseHeader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+interface ExerciseHeader{
+    title: string;
+    instruction: string;
+}
+const ExerciseHeader: React.FC<ExerciseHeader> = ({ title, instruction }) => {
+    return (
+        <header>
+            <h2 className="text-2xl md:text-3xl font-bold text-indigo-800 dark:text-indigo-100 mb-2 md:mb-8">{title}</h2>
+            <p className="text-lg md:text-2xl font-bold text-indigo-400 dark:text-indigo-200 mb-2 md:mb-4">
+                Task Description
+            </p>
+            <p className="text-sm md:text-base text-orange-500 dark:text-orange-500 bg-stone-50 dark:bg-[#484848] shadow-inner p-3 md:p-5 mb-4 rounded-lg">
+                {instruction}
+            </p>
+        </header>
+    );
+}
+
+export default ExerciseHeader;
