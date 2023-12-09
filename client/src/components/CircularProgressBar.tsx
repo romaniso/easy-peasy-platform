998504fc4f6e4a9d812facfb421c18ac6c4dde13
Roman Isopenko <isopenkoroman@gmail.com>
2023-12-09 17:39:42 +0100

    refactor: migrate CircularProgressBar to TypeScript

---
+++ client/src/components/CircularProgressBar.tsx
@@ -0,0 +1,51 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import CircleAnimation from './CircleAnimation';
+
+interface CircularProgressBarProps {
+    percentage: number;
+    offset: number;
+}
+
+const CircularProgressBar: React.FC<CircularProgressBarProps> = ({ percentage, offset }) => {
+    const resultRef = useRef<HTMLDivElement>(null);
+
+    const animatePercentage: React.EffectCallback = useCallback(() => {
+        if (resultRef.current) {
+            let counter = 0;
+            resultRef.current.innerHTML = counter + "%";
+
+            const intervalId = setInterval(() => {
+                if (counter === percentage) {
+                    clearInterval(intervalId);
+                } else {
+                    counter++;
+                    resultRef.current!.innerHTML = counter + "%";
+                }
+            }, 20);
+
+            return () => {
+                clearInterval(intervalId);
+            };
+        }
+    }, [percentage]);
+
+    useEffect(animatePercentage, [animatePercentage]);
+
+    return (
+        <div className="progress-bar w-[65px] h-[65px] relative">
+            <div className="outer h-[65px] w-[65px] rounded-full border p-2">
+                <div className="inner h-[45px] w-[45px] rounded-full flex items-center justify-center">
+                    <div
+                        ref={resultRef}
+                        className="font-bold text-indigo-900 text-sm dark:text-indigo-200"
+                    >
+                        {`${percentage ?? '0'}%`}
+                    </div>
+                </div>
+            </div>
+            <CircleAnimation offset={offset} />
+        </div>
+    );
+}
+
+export default CircularProgressBar;
