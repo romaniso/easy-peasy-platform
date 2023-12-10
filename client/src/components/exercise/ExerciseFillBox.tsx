3f2bc17534c5e9400a1ae320f003d64828a927e1
Roman Isopenko <isopenkoroman@gmail.com>
2023-12-10 21:00:23 +0100

    refactor: migrate ExerciseFillBox to TypeScript

---
+++ client/src/components/exercise/ExerciseFillBox.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {UserResult} from "../../types/userResult";
+import {ExerciseUnit} from "../../interfaces/exerciseUnit";
+
+interface ExerciseFillBoxProps {
+    questions: ExerciseUnit[];
+    text: string;
+    results: UserResult[] | null;
+    onChange(index: number, e: string): void;
+}
+const ExerciseFillBox: React.FC<ExerciseFillBoxProps> = ({questions, text, results, onChange}) => {
+    const handleText = (text: string) => {
+        return text.split('***').map((part: string, index: number, arr: string[]) => {
+            let inputColor: string = "";
+            if (results) {
+                inputColor = results[index] === "Same" ? 'bg-green-200 dark:bg-green-500/30 font-bold' : 'bg-red-200 dark:bg-red-500/30 font-bold text-black';
+            }
+            return (
+                <React.Fragment key={index}>
+                    <span>{part}</span>
+                    {index !== arr.length - 1 && (
+                        <span className='inline-block' key={`input-${index}`}>
+                            <span className='mr-1 bg-orange-400 dark:bg-orange-500 text-white py-[2px] md:py-1 px-2 md:px-3 rounded-lg text-base shadow'>{index + 1}</span>
+                            <input
+                                className={`text-base md:text-xl h-6 w-28 md:h-8 md:w-36 p-1 mr-1 border dark:border-gray-500 dark:bg-[#323232] rounded-md shadow-inner text-indigo-800 dark:text-indigo-50 outline-none hover:border-orange-400 focus:border-orange-300 focus:dark:border-orange-500 hover:dark:border-orange-500 transition-colors duration-700 ${inputColor}`}
+                                key={`input-field-${index}`}
+                                name="selection"
+                                type="text"
+                                autoComplete="off"
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(index, e.target.value)}
+                            />
+                        </span>
+                    )}
+                </React.Fragment>
+            );
+        });
+    };
+
+    const renderWords = questions.map(({word}, index) => (
+        <span key={index} className="text-base md:text-lg leading-8 font-bold text-indigo-700 dark:text-indigo-400 mr-6">
+      {word}
+    </span>
+    ));
+    const renderText = handleText(text);
+
+    return (
+        <div>
+            <div className="border dark:border-gray-500 rounded-md p-2 md:p-4 shadow mb-4 md:mb-6">{renderWords}</div>
+            <p className='text-base md:text-xl text-indigo-900 dark:text-indigo-200 md:leading-loose mb-8 text-justify'>{renderText}</p>
+        </div>
+    );
+}
+
+export default ExerciseFillBox;
