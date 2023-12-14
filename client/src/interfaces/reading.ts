fc1f7ba20b465afd9466404a366e74890a70d355
Roman Isopenko <isopenkoroman@gmail.com>
2023-12-14 17:28:29 +0100

    feat: add basic fetch for Reading section from frontend

---
+++ client/src/interfaces/reading.ts
@@ -0,0 +1,10 @@
+import {Level} from "../types/level";
+
+export interface IReading {
+    level: Level;
+    markDown: string;
+    setId: string;
+    topic: string;
+    _id: string;
+    image: string;
+}
--- client/src/pages/ExercisePage.tsx
+++ client/src/pages/ExercisePage.tsx
@@ -7,12 +7,13 @@ import ExerciseSet from "../components/exercise/ExerciseSet";
 import Cheatsheet from "../components/Cheatsheet";
 import Breadcrumbs from "../components/Breadcrumbs";
 import axios from "axios";
-// import Reading from "../components/exercise/Reading";
+import Reading from "../components/exercise/Reading";
 import CustomSkeleton from "../components/Skeleton";
 // Types
 import {Section} from "../types/section";
 import {SingleExercise} from "../interfaces/singleExercise";
 import {ICheatsheet} from "../interfaces/cheatsheet";
+import {IReading} from "../interfaces/reading";
 // Just for a template;
 import Recommended from "../components/Recommended";
 //#endregion
@@ -27,6 +28,7 @@ const ExercisePage: React.FC = () => {
     const [section, setSection] = useState<Section | null>(null);
     const [exercises, setExercises] = useState<SingleExercise[]>(defaultExercises);
     const [cheatsheet, setCheatsheet] = useState<ICheatsheet | null>(null);
+    const [reading, setReading] = useState<IReading | null>(null);
     const [isLoading, setIsLoading] = useState<boolean>(true);
     const {pathname} = useLocation();
 
@@ -35,13 +37,17 @@ const ExercisePage: React.FC = () => {
         const setTitle = decodeURIComponent(pathname.split('/')[2]);
         const getExerciseSet = async () => {
             try {
-                const {data} = await axios.get<{exercises: ExerciseObject; cheatsheet: ICheatsheet}>(`http://localhost:5000/exercise/${setTitle}`);
-                const {exercises, cheatsheet} = data;
+                const {data} = await axios.get<{exercises: ExerciseObject; cheatsheet: ICheatsheet; reading: IReading}>(`http://localhost:5000/exercise/${setTitle}`);
+                const {exercises, cheatsheet, reading} = data;
                 setExercises(exercises.exercises);
                 setSection(exercises.section);
                 if(cheatsheet){
                     setCheatsheet(cheatsheet);
                 }
+                if(reading){
+                    setReading(reading);
+                }
+                console.log(reading)
                 setIsLoading(false);
             } catch (error) {
                 throw new Error('There is no such an exercise set');
@@ -52,19 +58,6 @@ const ExercisePage: React.FC = () => {
 
 
     const {topic} = useParams();
-    const text =  <>
-        <h4 className='text-2xl mb-1'>Making Friends</h4>
-        <p className='mb-3'>Hi there! Let me tell you about my friend, Sarah. We first met at work. Sarah was a <strong>colleague</strong>, and we started as <strong>acquaintances</strong>. We would say a quick "hello" in the office, but we didn't really know each other well. One day, we decided to grab lunch together, and that's when we started to get to know each other better. Soon, Sarah became my <strong>bestie</strong>, someone I could share everything with. It's amazing how friendships can grow from being colleagues to best friends.</p>
-
-        <h4 className='text-2xl mb-1'>Keeping in Touch</h4>
-        <p className='mb-3'>Life can get busy, and sometimes we <strong>lose touch with</strong> our friends. But it's essential to <strong>keep in touch</strong> to maintain a strong relationship. Even when Sarah and I changed jobs, we made a promise to keep in touch. We would regularly send messages, call each other, and make plans to <strong>hang out</strong>. It's important to make an effort to stay connected, especially when life gets busy.</p>
-
-        <h4 className='text-2xl mb-1'>Unexpected Meetings</h4>
-        <p className='mb-3'>One day, I <strong>bumped into</strong> Sarah at the supermarket. It was such a pleasant surprise! We hadn't seen each other for a while, and it was great to <strong>catch up</strong>. We decided to get together for coffee, and it felt like no time had passed since our last meeting. Sometimes, these unexpected <strong>encounters</strong> can bring back the joy of friendship.</p>
-
-        <h4 className='text-2xl mb-1'>Friendship Forever</h4>
-        <p className='mb-3'>In conclusion, building and maintaining relationships takes effort, but it's worth it. Whether it's getting to know someone at work, keeping in touch through messages and calls, or bumping into a friend unexpectedly, these experiences contribute to strong and lasting friendships. So, don't forget to <strong>reach out to</strong> your friends, make plans to hang out, and cherish the moments you spend together. After all, friends are there to support each other through thick and thin.</p>
-    </>
 
     //FIXME Do I actually need here conditional rendering? Only one thing which changes is min-h...
     let content;
@@ -98,8 +91,9 @@ const ExercisePage: React.FC = () => {
             case 'reading':
                 content = (
                     <Panel className="bg-white flex flex-col lg:flex-row justify-between !p-0">
-                        {/*<Reading text={text} title='Building and Maintaining Relationships' bgImage={RelationshipsImg}*/}
-                        {/*         level='A2'/>*/}
+                        {!!reading && <Reading
+                            text={reading.markDown} title={reading.topic} level={reading.level} image={reading.image}
+                        />}
                     </Panel>
                 )
                 break;
