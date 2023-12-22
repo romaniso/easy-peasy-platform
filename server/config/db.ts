efb6f6528ebb3e9b88df7699781a67b44243cfcb
Roman Isopenko <isopenkoroman@gmail.com>
2023-12-22 22:17:04 +0100

    refactor: migrate db.js to TS

---
+++ server/config/db.ts
@@ -0,0 +1,10 @@
+import {Collection, Db, MongoClient} from 'mongodb';
+
+export const client: MongoClient = new MongoClient('mongodb://127.0.0.1:27017/');
+
+export const db: Db = client.db('easy-peasy-db');
+export const section: Collection<Document> = db.collection('section');
+export const exerciseSet: Collection<Document>  = db.collection('exercise_set');
+export const exercise: Collection<Document>  = db.collection('exercise');
+export const cheatsheet: Collection<Document>  = db.collection('cheatsheet');
+export const reading: Collection<Document>  = db.collection('reading');
