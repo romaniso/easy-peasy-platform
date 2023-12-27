e438046f0fdf26440e56761160107225a8d0a985
Roman Isopenko <isopenkoroman@gmail.com>
2023-12-27 14:10:16 +0100

    feat: add the expand button to DictionarySection

--- client/src/components/DictionarySection.tsx
+++ client/src/components/DictionarySection.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import DictionaryUnit from "./DictionaryUnit";
 import { MdOutlineEditNote } from "react-icons/md";
 import {useSelectedWords} from "../context/ReadingContext";
+import {BsChevronCompactLeft, BsChevronCompactRight} from "react-icons/bs";
 
 const DictionarySection: React.FC = () => {
+    const [isExpanded, setIsExpanded] = useState(true);
     const selectedWord = useSelectedWords();
 
-    return <aside className='basis-full md:basis-1/4 px-3 lg:px-4 py-4 lg:py-4'>
+    return <aside className={`basis-full md:basis-1/4 px-3 lg:px-4 py-4 lg:py-4 relative transition-all duration-700 ${isExpanded ? 'lg:min-w-[600px] lg:w-1/2' : 'lg:max-w-[400px]'}`}>
+        <button onClick={() => setIsExpanded(!isExpanded)} className='invisible lg:visible text-lg border dark:border-gray-500 rounded shadow-md text-indigo-900 dark:text-indigo-200 p-4 absolute -top-2 left-0 bg-white dark:bg-[#323232] hover:bg-indigo-50 hover:dark:bg-[#202020] transition-colors -translate-x-1/2 z-10'>
+            {isExpanded ? <BsChevronCompactRight/> : <BsChevronCompactLeft/>}
+        </button>
         <header className='flex items-center justify-center gap-2'>
             <h3 className='text-xl md:text-2xl font-bold mb-1 text-indigo-500 drop-shadow'>Dictionary List </h3>
             <MdOutlineEditNote className='text-indigo-500 text-2xl md:text-3xl drop-shadow'/>
