72aaf8ae65423ca614f25296361b45f44c3879fa
Roman Isopenko <isopenkoroman@gmail.com>
2024-01-05 21:27:16 +0100

    refactor: add FillInUnit by extracting a doublicated code from ExerciseFill and ExerciseListenAndType

--- client/src/components/exercise/ExerciseFill.tsx
+++ client/src/components/exercise/ExerciseFill.tsx
@@ -3,6 +3,7 @@ import { FaRegThumbsUp, FaRegThumbsDown } from "react-icons/fa";
 import {ExerciseUnit} from "../../interfaces/exerciseUnit";
 import {UserResult} from "../../types/userResult";
 import OrderUnit from "../OrderUnit";
+import FillInUnit from "./FillInUnit";
 
 interface ExerciseFillProps {
     questions: ExerciseUnit[];
@@ -16,45 +17,17 @@ const ExerciseFill: React.FC<ExerciseFillProps> = ({ questions, results, onChang
                 const feedbackIcon =
                     results &&
                     (results[index] === "Same" ? (
-                        <FaRegThumbsUp className="inline-block text-green-500 ml-2" />
+                        <FaRegThumbsUp className="inline-block text-green-500 ml-2 self-center" />
                     ) : (
-                        <FaRegThumbsDown className="inline-block ml-2 text-red-400" />
+                        <FaRegThumbsDown className="inline-block ml-2 text-red-400 self-center" />
                     ));
 
                 const { question } = item;
                 const renderedQuestion = (
                     <li className="text-indigo-900 dark:text-indigo-200 text-xl mb-8 flex items-start" key={index}>
                         <OrderUnit orderNumber={index + 1}/>
-                        <span className='-mt-0.5'>
-                            {question.split("***").map((part, partIndex) => {
-                                let renderedPart: React.ReactElement = <span key={'renderedPart-' + index}>{part}</span>;
-                                const HINT_DIVIDER = '(';
-                                if(part.includes(HINT_DIVIDER)){
-                                    const partArr: string[] = part.split(HINT_DIVIDER);
-                                    renderedPart = <span key={'renderedPart-' + index}>{partArr[0]}<span className='italic ml-1 text-orange-500 font-thin leading-8'>{HINT_DIVIDER}{partArr[1]}</span></span>
-                                }
-                            //Conditional Render
-                            const inputPart = (
-                                <span key={`${'input-' + partIndex} ${index}`}>
-                                    <input
-                                        className="text-xl p-1 border dark:border-gray-500 dark:bg-stone-800 rounded-md shadow-inner text-indigo-800 dark:text-indigo-200 outline-none"
-                                        key={index}
-                                        name="selection"
-                                        type="text"
-                                        autoComplete="off"
-                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(index, e.target.value)}
-                                    />
-                                    {renderedPart}
-                                    {feedbackIcon}
-                                </span>
-                            );
-                            return partIndex === 1 ? (
-                                inputPart
-                            ) : (
-                                renderedPart
-                            );
-                        })}
-                        </span>
+                        <FillInUnit unit={question} index={index} onChange={onChange}/>
+                        {feedbackIcon}
                     </li>
                 );
 
--- client/src/components/exercise/ExerciseListenAndType.tsx
+++ client/src/components/exercise/ExerciseListenAndType.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import {FaRegThumbsDown, FaRegThumbsUp} from "react-icons/fa";
 import OrderUnit from "../OrderUnit";
 import WaveFormPlayer from "../WaveFormPlayer";
+import FillInUnit from "./FillInUnit";
 
 interface ExerciseListenAndTypeProps {
     questions: ExerciseUnit[];
@@ -28,37 +29,9 @@ const ExerciseListenAndType: React.FC<ExerciseListenAndTypeProps> = ({questions,
                         <OrderUnit orderNumber={index + 1}/>
                         <WaveFormPlayer audioUrl={audioUrl as string} className='md:w-1/3 mb-8 mx-5 -mt-2 flex-grow md:flex-grow-0'/>
                         <div className='-mt-2 w-full basis-full'>
-                            <div className='border dark:border-indigo-50/20 p-5 rounded-md leading-loose bg-indigo-50 dark:bg-stone-800 shadow-md'>
-                                <span className='-mt-0.5'>
-                                    {question.split("***").map((part, partIndex) => {
-                                        let renderedPart: React.ReactElement = <span key={'renderedPart-' + index}>{part}</span>;
-                                        const HINT_DIVIDER = '(';
-                                        if(part.includes(HINT_DIVIDER)){
-                                            const partArr: string[] = part.split(HINT_DIVIDER);
-                                            renderedPart = <span key={'renderedPart-' + index}>{partArr[0]}<span className='italic ml-1 text-orange-500 font-thin leading-8'>{HINT_DIVIDER}{partArr[1]}</span></span>
-                                        }
-                                        //Conditional Render
-                                        const inputPart = (
-                                            <span key={`${'input-' + partIndex} ${index}`}>
-                                            <input
-                                                className="text-xl p-1 border dark:border-gray-500 dark:bg-stone-800 rounded-md shadow-inner text-indigo-800 dark:text-indigo-200 outline-none"
-                                                key={index}
-                                                name="selection"
-                                                type="text"
-                                                autoComplete="off"
-                                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => onChange(index, e.target.value)}
-                                            />
-                                                {renderedPart}
-                                                {feedbackIcon}
-                                        </span>
-                                        );
-                                        return partIndex === 1 ? (
-                                            inputPart
-                                        ) : (
-                                            renderedPart
-                                        );
-                                    })}
-                                </span>
+                            <div className='border dark:border-indigo-50/20 p-5 rounded-md leading-loose bg-indigo-50 dark:bg-stone-800 shadow-md text-lg'>
+                                <FillInUnit unit={question} index={index} onChange={onChange}/>
+                                {feedbackIcon}
                             </div>
                         </div>
                     </li>
