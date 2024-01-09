6b520c062fc580a25480a34fb73d86e9955a7ba6
Roman Isopenko <isopenkoroman@gmail.com>
2024-01-09 23:00:09 +0100

    refactor: add start function to root script

--- server/app.ts
+++ server/app.ts
@@ -3,19 +3,29 @@ import {Application} from "express";
 import cors from 'cors';
 import {sectionRouter} from "./src/routes/section";
 import {exerciseRouter} from "./src/routes/exercise";
+// const PORT = process.env.PORT || 5000;
 
 
 const app: Application = express();
 
+const start = () => {
+    try {
+        app.listen(5000, 'localhost', () => {
+            console.log('Server listening on port http://localhost:5000');
+        })
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+// MIDDLEWARES
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
 app.use('/section', sectionRouter);
 app.use('/exercise', exerciseRouter);
 
-app.listen(5000, 'localhost', () => {
-    console.log('Server listening on port http://localhost:5000');
-})
+start();
 
 
 
