fa7e38f89b32e9aaf71d9d694ffea35bd17c56d9
Roman Isopenko <isopenkoroman@gmail.com>
2024-01-14 12:24:18 +0100

    style: add plain styles to UnauthorizedPage

--- client/src/pages/UnauthorizedPage.tsx
+++ client/src/pages/UnauthorizedPage.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import useTop from "../hooks/useTop";
+import {useNavigate} from "react-router-dom";
+import Button from "../components/Button";
 
 const UnauthorizedPage: React.FC = () =>{
+    const navigate = useNavigate();
+
+    const goBack = () => navigate(-1);
     useTop();
     return (
-        <div className="min-h-screen flex justify-center items-center">
-            You are not authorized!
+        <div className="min-h-screen flex flex-col justify-center items-center">
+            <h1 className='text-orange-500 text-3xl drop-shadow mb-10'>You are not authorized!</h1>
+            <Button primary rounded onClick={goBack}>Go Back</Button>
         </div>
     );
 }
