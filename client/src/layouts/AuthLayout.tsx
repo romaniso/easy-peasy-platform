c42faf6c4a7515f65908ad5920c5aed6fc474e71
Roman Isopenko <isopenkoroman@gmail.com>
2024-01-22 22:33:40 +0100

    feat: add AuthLayout

---
+++ client/src/layouts/AuthLayout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+
+const AuthLayout: React.FC = () => {
+    return (
+        <div className="bg-gradient-to-r from-[#F5F3FF] dark:from-[#202020] via-[#FAE8FF] dark:via-[#3b4058] to-[#C7D2FE] dark:to-[#202020] overflow-hidden flex min-h-screen">
+            <Sidebar />
+            <main className="flex-1">
+                <Outlet />
+            </main>
+        </div>
+    );
+};
+
+export default AuthLayout;
