6ccc471829a101766586cf31328f4a46d8b1934b
Roman Isopenko <isopenkoroman@gmail.com>
2024-01-22 22:34:32 +0100

    refactor: restructure RootLayout, create AuthLayout cases with condition rendering

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,23 +1,32 @@
-import React, {ReactNode} from "react";
+import React from "react";
 import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import AuthLayout from "./AuthLayout";
+import useAuth from "../hooks/useAuth";
 
-interface RootLayoutProps {
-    children?: ReactNode;
-}
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
-    const { pathname  }  = useLocation();
-    return (
-        <div className="bg-gradient-to-r from-[#F5F3FF] dark:from-[#202020] via-[#FAE8FF] dark:via-[#3b4058] to-[#C7D2FE] dark:to-[#202020] overflow-hidden flex flex-col min-h-screen">
-            {pathname !== "/dashboard" && <Navbar />}
-            <main className="flex-1">
-                <Outlet/>
-                {/*{children}*/}
-            </main>
-            {pathname !== "/dashboard" && <Footer />}
-        </div>
-    );
-}
+const MODIFIED_PATHS_BY_AUTH = new Set(['/dashboard', '/profile', '/settings']);
+
+const RootLayout: React.FC = () => {
+    const { auth } = useAuth();
+    const { pathname } = useLocation();
+    let layoutContent: React.ReactNode;
+
+    if (MODIFIED_PATHS_BY_AUTH.has(pathname) && auth.accessToken) {
+        layoutContent = <AuthLayout />;
+    } else {
+        layoutContent = (
+            <div className="bg-gradient-to-r from-[#F5F3FF] dark:from-[#202020] via-[#FAE8FF] dark:via-[#3b4058] to-[#C7D2FE] dark:to-[#202020] overflow-hidden flex flex-col min-h-screen">
+                <Navbar />
+                <main className="flex-1">
+                    <Outlet />
+                </main>
+                <Footer />
+            </div>
+        );
+    }
+
+    return layoutContent;
+};
 
 export default RootLayout;
--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import useTop from "../hooks/useTop";
-import Sidebar from "../components/Sidebar";
 
 const DashboardPage: React.FC = () => {
     useTop();
     return (
-        <div className="flex">
-            <Sidebar />
-            <div className="p-7">
-                <h1 className="font-extrabold text-6xl text-center">
-                    Welcome to Dashboard
-                </h1>
-            </div>
+        <div className="p-7">
+            <h1 className="font-extrabold text-6xl text-center">
+                Welcome to Dashboard
+            </h1>
         </div>
     );
 }
