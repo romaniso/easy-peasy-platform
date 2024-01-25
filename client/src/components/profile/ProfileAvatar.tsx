c7a15aeed239dd7621af1d6817d4657ea371c76d
Roman Isopenko <isopenkoroman@gmail.com>
2024-01-25 18:16:24 +0100

    style: put responsive styles on ProfilePage

--- client/src/components/profile/ProfileAvatar.tsx
+++ client/src/components/profile/ProfileAvatar.tsx
@@ -3,7 +3,7 @@ import {SlPicture} from "react-icons/sl";
 import React from "react";
 const ProfileAvatar: React.FC = () => {
     return (
-        <div className='w-[250px] h-[250px] overflow-hidden rounded-full flex-shrink-0 relative shadow-md border border-indigo-200 dark:border-indigo-800 group'>
+        <div className='w-[100px] h-[100px] md:w-[230px] md:h-[230px] overflow-hidden rounded-full flex-shrink-0 relative shadow-md border border-indigo-200 dark:border-indigo-800 group'>
             <img src="https://avatar.iran.liara.run/public/boy" alt="" className='w-full h-full object-cover group-hover:brightness-50 transition-all duration-300'/>
             <button className='absolute bottom-0 inset-x-0 h-1/4 bg-black/50 flex justify-center items-center group-hover:h-[80px] transition-all duration-300 group-hover:bg-black/70'>
                 <ToolTip tooltip='Upload your photo'>
--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import ProfileAvatar from "../components/profile/ProfileAvatar";
 import PersonalDetails from "../components/profile/PersonalDetails";
+import Panel from "../components/Panel";
+import ProfileAvatar from "../components/profile/ProfileAvatar";
 
 const ProfilePage: React.FC = () => {
     return (
-        <section className="p-7 container mx-auto mt-6">
-            <h1 className="font-extrabold text-4xl text-center text-orange-500 mb-4">
-                Your Profile
-            </h1>
-            <div className='flex gap-8 mb-6'>
-                <ProfileAvatar/>
+        <div className='h-full md:p-10'>
+            <Panel className='bg-white flex md:flex-row flex-col !p-0 rounded-none md:rounded-md overflow-hidden h-full w-full'>
+                <div className='flex-shrink h-1/5 md:h-full md:basis-2/6 flex flex-col justify-center items-center bg-gradient-to-r from-orange-300 to-orange-200 dark:from-stone-800 dark:to-stone-600 p-3 md:p-5'>
+                    <ProfileAvatar/>
+                    <p className='text-center mt-1 md:mt-10 text-xl md:text-3xl text-indigo-800 dark:text-indigo-300 font-extrabold drop-shadow'>User Name</p>
+                    <blockquote className='text-center mt-10 text-lg text-indigo-800 dark:text-indigo-200 border-t border-indigo-600/50 dark:border-indigo-200/50 pt-8 italic md:block hidden'>
+                        "Those who know many languages live as many lives <br/> as the languages they know."
+                        <footer className='text-right mt-2 font-thin text-sm'>Czech proverb</footer>
+                    </blockquote>
+                </div>
                 <PersonalDetails/>
-            </div>
-        </section>
+            </Panel>
+        </div>
+
     );
 }
 
