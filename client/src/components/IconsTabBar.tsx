0465d3d274e2d090cacc21ac8314f5b5411bedb8
Roman Isopenko <isopenkoroman@gmail.com>
2024-01-27 11:06:04 +0100

    refactor: restyle IconsTabBar

--- client/src/components/IconsTabBar.tsx
+++ client/src/components/IconsTabBar.tsx
@@ -12,17 +12,19 @@ interface IconTabBarProps {
 
 const IconsTabBar: React.FC<IconTabBarProps> = ({ items, activeTab, setActiveTab, className}) => {
     const handleTab = (tab: string): void => {
+        console.log(tab);
         setActiveTab(tab);
     };
 
     const renderedTabs = items.map((item, index) => {
         return (
             <div className={`
-                basis-1/${items.length} flex-grow-0 flex flex-col justify-between items-center relative text-center after:[content: ""] before:[content: ""] after:transition-colors after:duration-300 before:transition-colors before:duration-300
+                basis-1/${items.length} flex-grow flex flex-col justify-between items-center relative text-center after:[content: ""] before:[content: ""] after:transition-colors after:duration-300 before:transition-colors before:duration-300
                 ${index !== (items.length - 1) && 'after:absolute after:w-1/2 after:top-1/2 after:right-0 after:h-1 after:-translate-y-5 z-0'}
-                ${index !== 0 && 'before:absolute before:w-1/2 before:top-1/2 before:left-0 before:bg-orange-500 before:h-1 before:-translate-y-5'}
-                ${(item.label === activeTab && index === 0 ) || items[index - 1]?.label === activeTab && index !== 0 ? 'before:bg-orange-500' : 'before:bg-indigo-400'}
-                ${item.label === activeTab ? 'after:bg-orange-500' : 'after:bg-indigo-400'}
+                ${index !== 0 && 'before:absolute before:w-1/2 before:top-1/2 before:left-0 before:h-1 before:-translate-y-5'}
+                
+                ${(item.label === activeTab) ? 'after:bg-orange-500' : 'after:bg-indigo-400'}
+                ${(items[index - 1]?.label === activeTab) ? 'before:bg-orange-500' : 'before:bg-indigo-400'}
                 `}
                  key={item.label}
             >
