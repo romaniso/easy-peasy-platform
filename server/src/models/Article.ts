9a03fbcc5c181e760e24070dd8e2d59a8666ac20
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-12 21:45:40 +0100

    feat: add Article shema, section and level types

---
+++ server/src/models/Article.ts
@@ -0,0 +1,28 @@
+import { ObjectId } from "mongodb";
+import { Schema, model } from "mongoose";
+import { Level } from "../types/level";
+import { Section } from "../types/section";
+
+export interface IArticle {
+  id: ObjectId;
+  title: string;
+  introduction: string;
+  level: Level;
+  section: Section;
+  readingTime?: number;
+  data: string;
+  setId?: ObjectId;
+}
+
+const articleSchema = new Schema<IArticle>({
+  id: { type: Schema.Types.ObjectId, unique: true, required: true },
+  title: { type: String, required: true },
+  introduction: { type: String, required: true },
+  level: { type: String, required: true },
+  section: { type: String, required: true },
+  readingTime: { type: Number, required: false },
+  data: { type: String, required: true },
+  setId: { type: Schema.Types.ObjectId, required: false },
+});
+
+export const Article = model<IArticle>("User", articleSchema);
---
+++ server/src/types/level.ts
@@ -0,0 +1,1 @@
+export type Level = "A1" | "A2" | "B1" | "B2" | "C1" | "C2" | "All";
