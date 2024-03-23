6b07cd7b71a4927d0139084178d984ad642a666d
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-23 13:05:13 +0100

    refactor: add event listener remover to ScrollToTop

--- client/src/components/common/ScrollToTop.tsx
+++ client/src/components/common/ScrollToTop.tsx
@@ -4,13 +4,16 @@ import { RxChevronUp } from "react-icons/rx";
 const ScrollToTop = () => {
   const [showTopBtn, setShowTopBtn] = useState(false);
   useEffect(() => {
-    window.addEventListener("scroll", () => {
+    const onScroll = (): void => {
       if (window.scrollY > 400) {
         setShowTopBtn(true);
       } else {
         setShowTopBtn(false);
       }
-    });
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return window.removeEventListener("scroll", onScroll);
   }, []);
   const goToTop = () => {
     window.scrollTo({
@@ -20,13 +23,12 @@ const ScrollToTop = () => {
   };
   return (
     <div className="relative">
-      {" "}
       {showTopBtn && (
         <RxChevronUp
           className="fixed bottom-10 right-6 z-50 icon-style rounded-md bg-indigo-500 text-indigo-50 cursor-pointer transition-transform ease-in-out duration-500 h-10 w-10 shadow-md"
           onClick={goToTop}
         />
-      )}{" "}
+      )}
     </div>
   );
 };
