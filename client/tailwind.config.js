7813d80e9a7b0bd525e3851e7c0f14b1a671526f
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-26 11:49:38 +0100

    fix: resolve tailwind-scrollbar issue

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,32 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./src/pages/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}"
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       dropShadow: {
         purpleGlow: [
           "0 0px 10px rgba(165, 180, 252, 0.2)",
-          "0 10px 5px rgba(165, 180, 252, 0.2)"
-        ]
+          "0 10px 5px rgba(165, 180, 252, 0.2)",
+        ],
       },
       keyframes: {
         slidein: {
-          '0%': {transform: 'translateX(100%)'},
-          '60%': {transform: 'translateX(-15%)'},
-          '80%': {transform: 'translateX(5%)'},
-          '100%': {transform: 'translateX(0%)'},
-        }
+          "0%": { transform: "translateX(100%)" },
+          "60%": { transform: "translateX(-15%)" },
+          "80%": { transform: "translateX(5%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
       },
       animation: {
-        'slidein-toast': 'slidein 0.4s linear forwards',
-      }
+        "slidein-toast": "slidein 0.4s linear forwards",
+      },
     },
-
   },
   plugins: [require("tailwind-scrollbar")],
-}
+};
