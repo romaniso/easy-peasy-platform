87fa836fe3c33ec35164ae3f190679c3fb75d5f5
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-28 21:03:59 +0100

    refactor: adjust sectionController to mongoose schema approach

--- server/src/controllers/sectionController.ts
+++ server/src/controllers/sectionController.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
-import { ExerciseSet } from "../models/ExerciseSet";
+import { ExerciseSet, IExerciseSet } from "../models/ExerciseSet";
+import { Section } from "../models/Section";
 
 export class SectionController {
   async getSetsBySection(req: Request, res: Response) {
     try {
       const { section } = req.params;
-      const exerciseSets = await ExerciseSet.findBySection(section);
+      const sectionId = (await Section.findOne({ name: section }))?._id;
+      const exerciseSets = await ExerciseSet.find({ sectionId });
+
+      if (!exerciseSets)
+        return res.status(404).json({ error: "No exercise sets were found." });
       return res.status(200).json(exerciseSets);
     } catch (err) {
       return res.status(500).json({ error: "Error finding section sets." });
