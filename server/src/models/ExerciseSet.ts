3f27b4c15b52a29297997f432a9c09f8fc887538
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-28 21:20:41 +0100

    refactor: migrate ExerciseSet to mongoose schema

--- server/src/models/ExerciseSet.ts
+++ server/src/models/ExerciseSet.ts
@@ -1,64 +1,26 @@
-import {ObjectId} from "mongodb";
-import {exerciseSet, section} from "../../config/db";
-// import {Section} from "./Section";
-export class ExerciseSet {
-    public _id: ObjectId;
-    public name: string;
-    public level: string;
-    public description: string;
-    public image: string;
-    public imgBase64?: string;
-    public sectionId: ObjectId;
-    constructor(obj: ExerciseSet) {
-        this._id = new ObjectId(obj._id);
-        this.name = obj.name;
-        this.level = obj.level;
-        this.description = obj.description;
-        this.image = obj.imgBase64 as string;
-        this.sectionId = new ObjectId(obj.sectionId);
-        // this._validate();
-    }
-    _validate(){
-        //Must be a proper validation
-    }
-    // static _checkRecord(record){
-    //     if(!(record instanceof ExerciseSet)) {
-    //         throw new Error('Record must exist and must be an instance of class ExerciseSet')
-    //     }
-    // }
-    async  insert(){
-        const {insertedId} = await exerciseSet.insertOne({
-            name: String(this.name),
-            level: String(this.level),
-            description: String(this.description),
-            image: String(this.image),
-            sectionId: new ObjectId(this.sectionId),
+import { ObjectId } from "mongodb";
+import { Schema, model } from "mongoose";
+import { Level } from "../types/level";
 
-        });
-        this._id = insertedId;
-
-        return insertedId;
-    }
-    async delete(){
-        await exerciseSet.deleteOne({
-            _id: this._id,
-        })
-    }
-    static async findById(id: string){
-        const item = await exerciseSet.findOne<ExerciseSet>({_id: new ObjectId(String(id))});
-        return item === null ? null : new ExerciseSet(item);
-    }
-    // static async findAll(){
-    //     const result = await exerciseSet.find();
-    //     const resultArray = await result.toArray();
-    //     return resultArray.map(obj => new ExerciseSet(obj));
-    // }
-    static async findBySection(chosenSection: string) {
-        const sectionId = (await section.findOne({name: chosenSection}))?._id;
-        return (await exerciseSet.find<ExerciseSet>({sectionId}).toArray()).map(obj => new ExerciseSet(obj));
-    }
-    static async findAllWithCursor() {
-        return /*await*/ exerciseSet.find();
-    }
-    async  update(){}
+export interface IExerciseSet {
+  _id: ObjectId;
+  name: string;
+  level: Level;
+  description: string;
+  imgBase64: string;
+  sectionId: ObjectId;
 }
+
+const exerciseSetSchema = new Schema<IExerciseSet>({
+  _id: { type: Schema.Types.ObjectId, unique: true, required: true },
+  name: { type: String, required: true },
+  level: { type: String, required: true },
+  description: { type: String, required: true },
+  imgBase64: { type: String, required: true },
+  sectionId: { type: Schema.Types.ObjectId, unique: true, required: true },
+});
+
+export const ExerciseSet = model<IExerciseSet>(
+  "ExerciseSet",
+  exerciseSetSchema
+);
