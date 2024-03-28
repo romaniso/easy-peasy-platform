16e4061b4340ef31f79b4c9ceb25e6a4846a20c6
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-28 21:06:18 +0100

    refactor: decompose getExerciseSet method logic from exerciseRouter to exerciseController

--- server/src/routes/exerciseRouter.ts
+++ server/src/routes/exerciseRouter.ts
@@ -1,15 +1,7 @@
-import express, {Request, Response, Router} from 'express';
-import {Exercise} from "../models/Exercise";
-import {Cheatsheet} from "../models/Cheatsheet";
-import  {Reading} from "../models/Reading";
-import {Listening} from "../models/Listening";
+import express, { Router } from "express";
+import { ExerciseController } from "../controllers/exerciseController";
 export const exerciseRouter: Router = express.Router();
 
-exerciseRouter
-    .get('/:chosen', async (req: Request, res: Response) => {
-        const exercises = await Exercise.findBySet(req.params.chosen);
-        const cheatsheet = await Cheatsheet.findBySetName(req.params.chosen);
-        const reading = await Reading.findBySet(req.params.chosen);
-        const listening = await Listening.findBySet(req.params.chosen);
-        res.json({exercises, cheatsheet, reading, listening});
-    })
+const controller = new ExerciseController();
+
+exerciseRouter.route("/:set").get(controller.getExerciseSet);
