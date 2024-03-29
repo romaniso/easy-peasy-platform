3c522e3abfa0436d8d65e98295aca188fdbf9fc5
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-29 11:13:27 +0100

    refactor: migrate Listening to mongoose schema

--- server/src/controllers/exerciseController.ts
+++ server/src/controllers/exerciseController.ts
@@ -14,30 +14,14 @@ export class ExerciseController {
         apiKey: set,
       })) as IExerciseSet;
       const { name: section } = (await Section.findById(sectionId)) as ISection; //section
-      const exercises = await Exercise.find({ setId }); // exercises
-      const cheatsheet = await Cheatsheet.findOne({ setId }); //cheatsheet
-      const reading = await Reading.findOne({ setId }); //reading
+      const exercises = await Exercise.find({ setId });
+      const cheatsheet = await Cheatsheet.findOne({ setId });
+      const reading = await Reading.findOne({ setId });
+      const listening = await Listening.findOne({ setId });
 
-      //const listening = await Listening.findBySet(set);
-      res.json({ exercises, section, cheatsheet, reading });
+      res.json({ exercises, section, cheatsheet, reading, listening });
     } catch (err) {
       return res.status(500).json({ error: "Error finding exercise set." });
     }
   }
-  //  static async getExercise(apiKey: string) {
-  //    try {
-  //      const { _id: setId, sectionId } = (await ExerciseSet.findOne({
-  //        apiKey,
-  //      })) as IExerciseSet;
-  //      // @fixme: refactor me please, it looks too robust
-  //      return {
-  //        section: (await Section.findById(sectionId))?.name,
-  //        exercises: (await exercise.find<Exercise>({ setId }).toArray()).map(
-  //          (obj) => new Exercise(obj)
-  //        ),
-  //      };
-  //    } catch (err) {
-  //      return res.status(500).json({ error: "Error finding exercise." });
-  //    }
-  //  }
 }
--- server/src/models/Listening.ts
+++ server/src/models/Listening.ts
@@ -1,53 +1,25 @@
-import {ObjectId} from "mongodb";
-import {exerciseSet, listening} from "../../config/db";
-import {ExerciseSet} from "./ExerciseSet";
-export class Listening {
-    public _id: ObjectId;
-    public topic: string;
-    public level: string;
-    public description: string;
-    public setId: ObjectId;
-    public image: string;
-    public audioUrl: string;
-    constructor(obj: Listening) {
-        this._id = new ObjectId(obj._id);
-        this.topic = obj.topic;
-        this.level = obj.level;
-        this.setId = new ObjectId(obj.setId);
-        this.image = obj.image;
-        this.description = obj.description;
-        this.audioUrl = obj.audioUrl;
-        // this._validate();
-    }
-    _validate(){
-        //Must be a proper validation
-    }
-    // static _checkRecord(record){
-    //     if(!(record instanceof Reading)) {
-    //         throw new Error('Record must exist and must be an instance of class Reading')
-    //     }
-    // }
-    async  insert(){
-    }
-    async delete(){
-    }
-    static async findById(id: string){
-        const item = await listening.findOne<Listening>({_id: new ObjectId(String(id))});
-        return item === null ? null : new Listening(item);
-    }
-    // static async findAll(){
-    //     const result = await reading.find();
-    //     const resultArray = await result.toArray();
-    //     return resultArray.map(obj => new Reading(obj));
-    // }
-    static async findBySet(chosenSet: string){
-        console.log(chosenSet);
-        const {_id: setId} = (await exerciseSet.findOne<ExerciseSet>({name: chosenSet}) as ExerciseSet);
-        const found = await listening.findOne<Listening>({setId});
-        return found ? new Listening(found) : null;
-    }
-    static async findAllWithCursor() {
-        return /*await*/ listening.find();
-    }
-    async  update(){}
+import { ObjectId } from "mongodb";
+import { Schema, model } from "mongoose";
+import { Level } from "../types/level";
+
+export interface IListening {
+  readonly _id: ObjectId;
+  topic: string;
+  level: Level;
+  description: string;
+  setId: ObjectId;
+  image: string;
+  audioUrl: string;
 }
+
+const listeningSchema = new Schema<IListening>({
+  _id: { type: Schema.Types.ObjectId, unique: true, required: true },
+  topic: { type: String, required: true },
+  level: { type: String, required: true },
+  description: { type: String, required: true },
+  setId: { type: Schema.Types.ObjectId, unique: true, required: true },
+  image: { type: String, required: false },
+  audioUrl: { type: String, required: false },
+});
+
+export const Listening = model<IListening>("Listening", listeningSchema);
