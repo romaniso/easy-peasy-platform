93d2fc45cb698ba816f953cff1872dc15ff9225a
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-29 10:34:48 +0100

    refactor: migrate Exercise to mongoose schema

---
+++ server/src/controllers/exerciseController.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { Exercise } from "../models/Exercise";
+import { Cheatsheet } from "../models/Cheatsheet";
+import { Reading } from "../models/Reading";
+import { Listening } from "../models/Listening";
+import { ExerciseSet, IExerciseSet } from "../models/ExerciseSet";
+import { ISection, Section } from "../models/Section";
+
+export class ExerciseController {
+  async getExerciseSet(req: Request, res: Response) {
+    try {
+      const { set } = req.params;
+      const { _id: setId, sectionId } = (await ExerciseSet.findOne({
+        apiKey: set,
+      })) as IExerciseSet;
+      const { name: section } = (await Section.findById(sectionId)) as ISection; //section
+      const exercises = await Exercise.find({ setId }); // exercises
+      //const cheatsheet = await Cheatsheet.findBySetName(set);
+      //const reading = await Reading.findBySet(set);
+      //const listening = await Listening.findBySet(set);
+      //res.json({ exercises, cheatsheet, reading, listening });
+      res.json({ exercises, section });
+    } catch (err) {
+      return res.status(500).json({ error: "Error finding exercise set." });
+    }
+  }
+  //  static async getExercise(apiKey: string) {
+  //    try {
+  //      const { _id: setId, sectionId } = (await ExerciseSet.findOne({
+  //        apiKey,
+  //      })) as IExerciseSet;
+  //      // @fixme: refactor me please, it looks too robust
+  //      return {
+  //        section: (await Section.findById(sectionId))?.name,
+  //        exercises: (await exercise.find<Exercise>({ setId }).toArray()).map(
+  //          (obj) => new Exercise(obj)
+  //        ),
+  //      };
+  //    } catch (err) {
+  //      return res.status(500).json({ error: "Error finding exercise." });
+  //    }
+  //  }
+}
--- server/src/models/Exercise.ts
+++ server/src/models/Exercise.ts
@@ -1,73 +1,29 @@
-import {ObjectId} from "mongodb";
-import {exerciseSet, exercise} from "../../config/db";
-import {Section} from "./Section";
-import {ExerciseTypeName} from "../enums/exercise";
-import {ExerciseSet} from "./ExerciseSet";
+import { ObjectId } from "mongodb";
+import { Schema, model } from "mongoose";
+import { Level } from "../types/level";
+import { ExerciseTypeName } from "../enums/exercise";
 
-interface DbExerciseObject {
-    _id: ObjectId;
-    title: string;
-    instruction: string;
-    type: ExerciseTypeName;
-    setId: ObjectId;
-    data: {} | [];
-}
-export class Exercise {
-    private readonly _id: ObjectId;
-    private title: string;
-    private instruction: string;
-    private type: ExerciseTypeName;
-    private data: {} | [];
-    private setId: ObjectId;
-    constructor(obj: Exercise) {
-        this._id = new ObjectId(obj._id);
-        this.title = obj.title;
-        this.instruction = obj.instruction;
-        this.type = obj.type;
-        this.data = obj.data;
-        this.setId = new ObjectId(obj.setId);
-        // this._validate();
-    }
-    _validate(){
-        //Must be a proper validation
-    }
-    async  insert(){
-        // const {insertedId} = await exerciseSet.insertOne({
-        //     name: String(this.name),
-        //     level: String(this.level),
-        //     description: String(this.description),
-        //     image: String(this.image),
-        //     sectionId: new ObjectId(this.sectionId),
-        //
-        // });
-        // this._id = insertedId;
+type ExerciseDataType = {
+  units: [];
+  text?: string;
+};
 
-        // return insertedId;
-    }
-    async delete(){
-        // await exerciseSet.deleteOne({
-        //     _id: this._id,
-        // })
-    }
-    static async findById(id: string): Promise<Exercise | null>{
-        const item = await exercise.findOne<Exercise>({_id: new ObjectId(String(id))});
-        return item === null ? null : new Exercise(item);
-    }
-    // static async findAll(){
-    //     const result = await exercise.find();
-    //     const resultArray = await result.toArray();
-    //     return resultArray.map(obj => new Exercise(obj));
-    // }
-    static async findBySet(chosenSet: string){
-        const {_id: setId, sectionId} = (await exerciseSet.findOne<ExerciseSet>({name: chosenSet}) as ExerciseSet);
-        // @fixme: refactor me please, it looks too robust
-        return {
-            section: (await Section.findById(sectionId))?.name,
-            exercises: (await exercise.find<Exercise>({setId}).toArray()).map(obj => new Exercise(obj))
-        };
-    }
-    static async findAllWithCursor() {
-        return /*await*/ exercise.find();
-    }
-    async  update(){}
+export interface IExercise {
+  _id: ObjectId;
+  title: string;
+  instruction: Level;
+  type: ExerciseTypeName;
+  setId: ObjectId;
+  data: ExerciseDataType;
 }
+
+const exerciseSchema = new Schema<IExercise>({
+  _id: { type: Schema.Types.ObjectId, unique: true, required: true },
+  title: { type: String, required: true },
+  instruction: { type: String, required: true },
+  type: { type: String, required: true },
+  setId: { type: Schema.Types.ObjectId, unique: true, required: true },
+  data: { type: Object, required: true },
+});
+
+export const Exercise = model<IExercise>("Exercise", exerciseSchema);
