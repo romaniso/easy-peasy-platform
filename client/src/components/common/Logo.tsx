9bafaba033e0a8280a44bb1ae582e65470a63503
Roman Isopenko <isopenkoroman@gmail.com>
2024-03-31 11:54:58 +0200

    refactor: restyle Logo

--- client/src/components/common/Logo.tsx
+++ client/src/components/common/Logo.tsx
@@ -1,16 +1,20 @@
 import LogoImage from "../../assets/images/small-logo.png";
 
-export const Logo = () => {
+interface Props {
+  className?: string;
+}
+
+export const Logo = ({ className }: Props) => {
   return (
-    <span className="flex items-center group">
+    <span className={`flex items-center group ${className}`}>
       <img
         src={LogoImage}
         alt="logo"
         className="w-8 group-hover:scale-110 transition-transform duration-500"
       />
-      <span className="text-lg font-semibold dark:text-indigo-300 text-indigo-800 group-hover:drop-shadow-purpleGlow transition-all duration-500 whitespace-nowrap relative">
+      <span className="text-lg font-semibold dark:text-indigo-300 text-indigo-500 group-hover:drop-shadow-purpleGlow transition-all duration-500 whitespace-nowrap relative">
         EASY-PEASY
-        <small className="absolute right-0 -bottom-1.5 dark:text-orange-300 text-orange-700 rounded-md px-1 leading-none font-thin text-xs">
+        <small className="absolute right-0 -bottom-1.5 dark:text-orange-300 text-orange-500 rounded-md px-1 leading-none font-thin text-xs">
           beta
         </small>
       </span>
