4a87cecd8791ea39fc71c19e936f36b2f0bca94f
Roman Isopenko <isopenkoroman@gmail.com>
2024-04-05 23:23:09 +0200

    fix: try to resolve CORS issue by adding allowed origin

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const allowedOrigins = [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'http://192.168.0.88:5173',
-    // "https://actualdomain.com",
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "http://192.168.0.88:5173",
+  process.env.ALLOWED_ORIGIN,
 ];
--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,16 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import * as process from "process";
-import multer from 'multer';
+import multer from "multer";
 
 dotenv.config();
 
 export const config = {
-    //@TODO: generate a random default key instead of hardcoded
-    secretToken: process.env.ACCESS_TOKEN_SECRET || 'default_secret_key',
-    refreshToken: process.env.REFRESH_TOKEN_SECRET,
-    awsAvatarsBucketName: process.env.AWS_AVATARS_BUCKET_NAME,
-    awsAvatarsBucketRegion: process.env.AWS_AVATARS_BUCKET_REGION,
-    awsAccessKey: process.env.AWS_ACCESS_KEY,
-    awsSecretKey: process.env.AWS_SECRET_KEY,
-    upload: multer({dest: 'uploads/'})
+  //@TODO: generate a random default key instead of hardcoded
+  secretToken: process.env.ACCESS_TOKEN_SECRET || "default_secret_key",
+  refreshToken: process.env.REFRESH_TOKEN_SECRET,
+  awsAvatarsBucketName: process.env.AWS_AVATARS_BUCKET_NAME,
+  awsAvatarsBucketRegion: process.env.AWS_AVATARS_BUCKET_REGION,
+  awsAccessKey: process.env.AWS_ACCESS_KEY,
+  awsSecretKey: process.env.AWS_SECRET_KEY,
+  upload: multer({ dest: "uploads/" }),
 };
