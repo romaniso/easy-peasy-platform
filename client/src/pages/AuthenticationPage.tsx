0a7f4388c7a02e907661a83d650adddd81f316eb
Roman Isopenko <isopenkoroman@gmail.com>
2024-04-10 23:06:19 +0200

    feat: add bg image to AuthenticationPage

--- client/src/pages/AuthenticationPage.tsx
+++ client/src/pages/AuthenticationPage.tsx
@@ -2,23 +2,29 @@ import React, { useState } from "react";
 import useTop from "../hooks/useTop";
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
+import BgImage from "../assets/images/bg-auth.jpg";
 
 const AuthenticationPage: React.FC = () => {
-    useTop();
-    const [toggle, setToggle] = useState(true);
+  useTop();
+  const [toggle, setToggle] = useState(true);
 
-    const handleToggle = () => {
-        setToggle(!toggle);
-    };
-    return (
-        <section className="min-h-screen flex items-center justify-center">
-            {toggle ? (
-                <Login onToggleForm={handleToggle} />
-            ) : (
-                <Register onToggleForm={handleToggle} />
-            )}
-        </section>
-    );
-}
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+  return (
+    <section className="min-h-screen flex items-center justify-center relative overflow-hidden">
+      <img
+        src={BgImage}
+        alt="background image"
+        className="absolute inset-0 w-full object-fill opacity-10 dark:opacity-5"
+      />
+      {toggle ? (
+        <Login onToggleForm={handleToggle} />
+      ) : (
+        <Register onToggleForm={handleToggle} />
+      )}
+    </section>
+  );
+};
 
 export default AuthenticationPage;
