adf6d90465a7d02f140d5f6b6414006dd84d2392
Roman Isopenko <isopenkoroman@gmail.com>
2024-04-11 13:41:20 +0200

    fix: revert bg-image to Loader and AuthenticationPage

--- client/src/pages/AuthenticationPage.tsx
+++ client/src/pages/AuthenticationPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import useTop from "../hooks/useTop";
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
-import BgImage from "../assets/images/bg-auth.jpg";
+//import BgImage from "../assets/images/bg-auth.jpg";
 
 const AuthenticationPage: React.FC = () => {
   useTop();
@@ -12,12 +12,12 @@ const AuthenticationPage: React.FC = () => {
     setToggle(!toggle);
   };
   return (
-    <section className="min-h-screen flex items-center justify-center relative overflow-hidden">
-      <img
+    <section className="min-h-screen flex items-center justify-center">
+      {/*<img
         src={BgImage}
         alt="background image"
         className="absolute inset-0 w-full object-fill opacity-10 dark:opacity-5"
-      />
+      />*/}
       {toggle ? (
         <Login onToggleForm={handleToggle} />
       ) : (
