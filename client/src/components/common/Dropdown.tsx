aeda4a91921227bd7a758dca8c1d5a14687c61e7
Roman Isopenko <isopenkoroman@gmail.com>
2024-04-14 10:52:51 +0200

    refactor: add isHome props to LanguageSwitcher, add conditional rendering

--- client/src/components/common/Dropdown.tsx
+++ client/src/components/common/Dropdown.tsx
@@ -1,106 +1,111 @@
-import React, {ReactElement, useEffect, useRef, useState} from "react";
-import {SubmenuItem} from "../Navbar";
+import React, { ReactElement, useEffect, useRef, useState } from "react";
+import { SubmenuItem } from "../Navbar";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
-import {NavLink} from "react-router-dom";
-import {AvatarItem} from "../ProfilePreview";
+import { NavLink } from "react-router-dom";
+import { AvatarItem } from "../ProfilePreview";
 
 interface Dropdown {
-    label: string;
-    content: SubmenuItem[] | AvatarItem[];
-    avatar?: true;
-    block?: true;
+  label: string | ReactElement;
+  content: SubmenuItem[] | AvatarItem[];
+  avatar?: true;
+  block?: true;
 }
-const Dropdown: React.FC<Dropdown> = ({label, content, avatar, block}) => {
-    const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
-    const dropdownRef = useRef<HTMLDivElement>(null);
+const Dropdown: React.FC<Dropdown> = ({ label, content, avatar, block }) => {
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        const handleOutsideClick = (event: MouseEvent) => {
-            if(!dropdownRef.current) return;
-            if(!dropdownRef.current.contains(event.target as Node)){
-                setIsCollapsed(false);
-            }
-        }
-        document.addEventListener('click', handleOutsideClick, true)
+  useEffect(() => {
+    const handleOutsideClick = (event: MouseEvent) => {
+      if (!dropdownRef.current) return;
+      if (!dropdownRef.current.contains(event.target as Node)) {
+        setIsCollapsed(false);
+      }
+    };
+    document.addEventListener("click", handleOutsideClick, true);
 
-        return () => document.removeEventListener('click', handleOutsideClick);
-    }, []);
+    return () => document.removeEventListener("click", handleOutsideClick);
+  }, []);
 
-    const icon: ReactElement = isCollapsed ? (
-        <FaChevronUp className="text-lg relative top-0.5" />
-    ) : (
-        <FaChevronDown className="text-lg relative top-0.5" />
-    );
-    return (
-        <div ref={dropdownRef} className='relative'>
-            <div className={
-                `cursor-pointer flex justify-between items-center 
-                ${block && 'p-2'} 
+  const icon: ReactElement = isCollapsed ? (
+    <FaChevronUp className="text-lg relative top-0.5" />
+  ) : (
+    <FaChevronDown className="text-lg relative top-0.5" />
+  );
+  return (
+    <div ref={dropdownRef} className="relative">
+      <div
+        className={`cursor-pointer flex justify-between items-center 
+                ${block && "p-2"} 
                 gap-2 relative 
-                ${avatar && 'w-[25vw] md:w-auto gap-0.5'}
+                ${avatar && "w-[25vw] md:w-auto gap-0.5"}
                 `}
-                 onClick={() => setIsCollapsed(!isCollapsed)
-            }>
-                <span>{label}</span>
-                <span>{icon}</span>
-            </div>
-            {isCollapsed && (
-                <div className={`
+        onClick={() => setIsCollapsed(!isCollapsed)}
+      >
+        <span>{label}</span>
+        <span>{icon}</span>
+      </div>
+      {isCollapsed && (
+        <div
+          className={`
                     block md:absolute top-full 
-                    ${block ? 'md:w-full p-2' : 'md:w-32 md:-ml-3'} 
+                    ${block ? "md:w-full p-2" : "md:w-32 md:-ml-3"} 
                     mt-2.5 dark:md:bg-stone-900/80 md:bg-white rounded-md md:shadow-md overflow-hidden 
-                    ${avatar && '-ml-14 md:!-ml-10 !w-[150%]'}`
-                }>
-                    <ul>
-                        {content.map((subItem) =>
-                            (subItem as AvatarItem).eventHandler
-                                ? (
-                                    <li className={`
+                    ${avatar && "-ml-14 md:!-ml-10 !w-[150%]"}`}
+        >
+          <ul>
+            {content.map((subItem) =>
+              (subItem as AvatarItem).eventHandler ? (
+                <li
+                  className={`
                                         text-indigo-600 dark:text-indigo-300 md:px-4 py-3 md:py-2 border-indigo-50/20 border-b hover:bg-indigo-50 dark:hover:bg-indigo-950 transition-colors duration-300 flex items-center gap-1 
-                                        ${(subItem as AvatarItem).isLogoutBtn && 'text-red-700 md:bg-red-500/20 hover:bg-red-500/40'}
-                                        ${avatar && '!px-1'}
+                                        ${
+                                          (subItem as AvatarItem).isLogoutBtn &&
+                                          "text-red-700 md:bg-red-500/20 hover:bg-red-500/40"
+                                        }
+                                        ${avatar && "!px-1"}
                                         `}
-                                        onClick={(subItem as AvatarItem).eventHandler}
-                                        key={subItem.label}
-                                    >
-                                        {subItem.icon && (
-                                            <span className='min-w-3'>
-                                                {subItem.icon}
-                                            </span>
-                                        )}
-                                        <span>
-                                            {subItem.label}
-                                        </span>
-
-                                    </li>
-                                )
-                            : (
-                                <NavLink key={subItem.label} to={(subItem as SubmenuItem).path} onClick={() => setIsCollapsed(!isCollapsed)}>
-                                    <li className={`
+                  onClick={(subItem as AvatarItem).eventHandler}
+                  key={subItem.label}
+                >
+                  {subItem.icon && (
+                    <span className="min-w-3">{subItem.icon}</span>
+                  )}
+                  <span>{subItem.label}</span>
+                </li>
+              ) : (
+                <NavLink
+                  key={subItem.label}
+                  to={(subItem as SubmenuItem).path}
+                  onClick={() => setIsCollapsed(!isCollapsed)}
+                >
+                  <li
+                    className={`
                                         text-indigo-600 dark:text-indigo-300 md:px-4 py-3 md:py-2 border-indigo-50/20 border-b hover:bg-indigo-50 dark:hover:bg-indigo-950 transition-colors duration-300 flex items-center gap-1 
-                                        ${(subItem as AvatarItem).isLogoutBtn && 'bg-red-500/20 hover:bg-red-500/40'} 
-                                        ${block && 'flex justify-between flex-row-reverse'}
-                                        ${avatar && '!px-1'}
-                                        `
-                                    }>
-                                        {subItem.icon && (
-                                            <span className='min-w-3'>
-                                                {subItem.icon}
-                                            </span>
-                                        )}
-                                        <span>
-                                            {subItem.label}
-                                        </span>
-                                    </li>
-                                </NavLink>
-                            )
-                        )}
-                    </ul>
-                </div>
+                                        ${
+                                          (subItem as AvatarItem).isLogoutBtn &&
+                                          "bg-red-500/20 hover:bg-red-500/40"
+                                        } 
+                                        ${
+                                          block &&
+                                          "flex justify-between flex-row-reverse"
+                                        }
+                                        ${avatar && "!px-1"}
+                                        `}
+                  >
+                    {subItem.icon && (
+                      <span className="min-w-3">{subItem.icon}</span>
+                    )}
+                    <span>{subItem.label}</span>
+                  </li>
+                </NavLink>
+              )
             )}
+          </ul>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Dropdown;
--- client/src/components/settings/LanguageSwitcher.tsx
+++ client/src/components/settings/LanguageSwitcher.tsx
@@ -1,53 +1,93 @@
-import {IoLanguage} from "react-icons/io5";
-import React, {ReactElement, useState} from "react";
-import PolandFlag from '../../assets/images/poland.png';
-import UsaFlag from '../../assets/images/usa.png';
-import UkraineFlag from '../../assets/images/ukraine.png';
+import { TbWorld } from "react-icons/tb";
+import { ReactElement, useState } from "react";
+import PolandFlag from "../../assets/images/poland.png";
+import UsaFlag from "../../assets/images/usa.png";
+import UkraineFlag from "../../assets/images/ukraine.png";
 import Select from "../common/Select";
-import {Language} from "../../enums/lang";
-import {useTranslation} from "react-i18next";
-import {ToastType} from "../../enums/toast";
-import {useToast} from "../../context/ToastContext";
+import { Language } from "../../enums/lang";
+import { useTranslation } from "react-i18next";
+import { ToastType } from "../../enums/toast";
+import { useToast } from "../../context/ToastContext";
+import { IoLanguage } from "react-icons/io5";
 
 interface LanguageSwitcherItem {
-    value: Language;
-    label: string;
-    icon: ReactElement;
+  value: Language;
+  label: string;
+  icon?: ReactElement;
 }
-const LanguageSwitcher: React.FC = () => {
-    const [, setSelectedLanguage] = useState<Language>((localStorage.getItem('i18nextLng') as Language) || Language.English);
-    const toast = useToast();
+interface Props {
+  isHome?: true;
+}
+export const LanguageSwitcher = ({ isHome }: Props) => {
+  const [selectedLanguage, setSelectedLanguage] = useState<Language>(
+    (localStorage.getItem("i18nextLng") as Language) || Language.English
+  );
+  const toast = useToast();
 
-    const {i18n} = useTranslation();
-    const {t} = useTranslation('settings');
+  const { i18n } = useTranslation();
+  const { t } = useTranslation("settings");
 
-    const languages: LanguageSwitcherItem[] = [
-        { value: Language.English, label: t('languages.en'), icon: <img src={UsaFlag} alt='American flag' className='w-7'/> },
-        { value: Language.Polish, label: t('languages.pl'), icon:  <img src={PolandFlag} alt='Polish flag' className='w-7'/> },
-        { value: Language.Ukrainian, label: t('languages.ua'), icon:  <img src={UkraineFlag} alt='Ukrainian flag' className='w-7'/> },
-    ]
+  const languages: LanguageSwitcherItem[] = [
+    {
+      value: Language.English,
+      label: isHome ? "EN" : t("languages.en"),
+      icon: isHome ? undefined : (
+        <img src={UsaFlag} alt="American flag" className="w-7" />
+      ),
+    },
+    {
+      value: Language.Polish,
+      label: isHome ? "PL" : t("languages.pl"),
+      icon: isHome ? undefined : (
+        <img src={PolandFlag} alt="Polish flag" className="w-7" />
+      ),
+    },
+    {
+      value: Language.Ukrainian,
+      label: isHome ? "UA" : t("languages.ua"),
+      icon: isHome ? undefined : (
+        <img src={UkraineFlag} alt="Ukrainian flag" className="w-7" />
+      ),
+    },
+  ];
 
-    const handleSwitchLanguage = (lang: Language) => {
-        i18n.changeLanguage(lang);
-        setSelectedLanguage(lang);
-        const toastMessage = t('selectLanguage.toastMessage');
-        if(toastMessage){
-            toast?.open(toastMessage, ToastType.Success);
-        }
+  const handleSwitchLanguage = (lang: Language) => {
+    i18n.changeLanguage(lang);
+    setSelectedLanguage(lang);
+    const toastMessage = t("selectLanguage.toastMessage");
+    if (toastMessage) {
+      toast?.open(toastMessage, ToastType.Success);
     }
-
+  };
+  if (isHome)
     return (
-        <section>
-            <h2 className='text-lg md:text-2xl text-indigo-500 dark:text-indigo-200 font-bold drop-shadow flex items-center gap-1 mb-3.5 md:mb-6'>
-                {t('subheadings.defaultLanguage')}
-                <IoLanguage/>
-            </h2>
-            <Select
-                options={languages}
-                onChange={handleSwitchLanguage}
-                defaultText={t('selectLanguage.selectInputText')}
-            />
-        </section>
-    )
-}
-export default LanguageSwitcher;
+      <section>
+        <Select
+          options={languages}
+          onChange={handleSwitchLanguage}
+          defaultText={
+            <div className="flex justify-center items-center gap-2">
+              <TbWorld className="text-xl" />
+              <span className="uppercase">{selectedLanguage}</span>
+            </div>
+          }
+          noBorders
+          noArrows
+        />
+      </section>
+    );
+
+  return (
+    <section>
+      <h2 className="text-lg md:text-2xl text-indigo-500 dark:text-indigo-200 font-bold drop-shadow flex items-center gap-1 mb-3.5 md:mb-6">
+        {t("subheadings.defaultLanguage")}
+        <IoLanguage />
+      </h2>
+      <Select
+        options={languages}
+        onChange={handleSwitchLanguage}
+        defaultText={t("selectLanguage.selectInputText")}
+      />
+    </section>
+  );
+};
