147e00dfc07c1b38b720c840743352e238cc682f
Roman Isopenko <isopenkoroman@gmail.com>
2024-05-01 14:58:29 +0200

    fix: resolve white spaces error in generating user's results

--- client/src/utils/generateUserResults.ts
+++ client/src/utils/generateUserResults.ts
@@ -1,21 +1,25 @@
 import findDifferentIndexesInArrays from "./findDifferentIndexesInArrays";
-import {UserResult} from "../types/userResult";
-import {ExerciseTypeName} from "../enums/exercise";
+import { UserResult } from "../types/userResult";
+import { ExerciseTypeName } from "../enums/exercise";
 
-function generateUserResults (userAnswers: string[], keys: (string | string[])[], type: ExerciseTypeName): UserResult[]  {
-    return userAnswers.map((answer, index) => {
-        const arrAnswer: string[] = answer.split("");
-        if (type === ExerciseTypeName.FillInLetter) {
-            const results: number[] = findDifferentIndexesInArrays(
-                keys[index] as string[],
-                arrAnswer
-            );
-            return results.length ? results : "Same";
-        }
-        return answer.toLowerCase() === (keys[index]).toString().toLowerCase()
-            ? "Same"
-            : "Different";
-    });
+function generateUserResults(
+  userAnswers: string[],
+  keys: (string | string[])[],
+  type: ExerciseTypeName
+): UserResult[] {
+  return userAnswers.map((answer, index) => {
+    const arrAnswer: string[] = answer.split("");
+    if (type === ExerciseTypeName.FillInLetter) {
+      const results: number[] = findDifferentIndexesInArrays(
+        keys[index] as string[],
+        arrAnswer
+      );
+      return results.length ? results : "Same";
+    }
+    return answer.toLowerCase().trim() === keys[index].toString().toLowerCase()
+      ? "Same"
+      : "Different";
+  });
 }
 
 export default generateUserResults;
