4f99f275d5c1d45cd99ea651c09a69bd14bace8a
Vadym Mariiechko <vadym.mariiechko@addepto.com>
2024-05-09 20:33:40 +0200

    refactor: review auth login flow

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -14,49 +14,50 @@ export class AuthController {
             "User with this username was not found. Try with a different user name.",
         });
       }
-      // evaluate password
-      const match = bcrypt.compareSync(password, foundUser.password);
+
+      // Evaluate password asynchronously
+      const match = await bcrypt.compare(password, foundUser.password);
       if (!match) {
         return res.status(401).json({ message: "Invalid password." });
-      } else {
-        // create JWT and refresh token
-        const accessToken = generateAccessToken(
-          foundUser.username,
-          foundUser.roles
-        );
-        const refreshToken = generateAccessToken(
-          foundUser.username,
-          foundUser.roles,
-          true
-        );
-        //save refreshToken to DB for this user
-        await User.updateOne(
-          { _id: foundUser._id },
-          { $set: { refreshToken } }
-        );
-        //httpOnly prevents saving it in JS
-        //@TODO: in production I need to add secure: true for https
-        res.cookie("jwt", refreshToken, {
-          httpOnly: true,
-          sameSite: "none",
-          secure: true,
-          maxAge: 24 * 60 * 60 * 1000,
-        }); //one day
-
-        const user = {
-          avatar: foundUser.avatar,
-          firstName: foundUser.firstName,
-          lastName: foundUser.lastName,
-          email: foundUser.email,
-          birthday: foundUser.birthday,
-          likes: foundUser.likes,
-          motivations: foundUser.motivations,
-          goals: foundUser.goals,
-        };
-
-        //@TODO: send user object for user context (non-sensitive data)
-        res.json({ accessToken, roles: foundUser.roles, user });
       }
+
+      // create JWT and refresh token
+      const accessToken = generateAccessToken(
+        foundUser.username,
+        foundUser.roles
+      );
+      const refreshToken = generateAccessToken(
+        foundUser.username,
+        foundUser.roles,
+        true
+      );
+      //save refreshToken to DB for this user
+      await User.updateOne(
+        { _id: foundUser._id },
+        { $set: { refreshToken } }
+      );
+      //httpOnly prevents saving it in JS
+      //@TODO: in production I need to add secure: true for https
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      }); //one day
+
+      const user = {
+        avatar: foundUser.avatar,
+        firstName: foundUser.firstName,
+        lastName: foundUser.lastName,
+        email: foundUser.email,
+        birthday: foundUser.birthday,
+        likes: foundUser.likes,
+        motivations: foundUser.motivations,
+        goals: foundUser.goals,
+      };
+
+      //@TODO: send user object for user context (non-sensitive data)
+      res.json({ accessToken, roles: foundUser.roles, user });
     } catch (err) {
       console.error(err);
       res.status(401).json({ message: "Login error" });
