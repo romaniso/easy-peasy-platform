3b4feda891c326ecd81b76eea082cad760c9682e
Roman Isopenko <isopenkoroman@gmail.com>
2024-08-26 22:52:58 +0200

    refactor: prettify validation in resetRoutes using express-validator

--- server/src/routes/resetRouter.ts
+++ server/src/routes/resetRouter.ts
@@ -5,14 +5,12 @@ import { ResetController } from "../controllers/resetController.js";
 export const resetRouter: Router = express.Router();
 const controller = new ResetController();
 
-resetRouter.post("/", controller.sendEmail);
+resetRouter.post("/", check("userEmail").isEmail(), controller.sendEmail);
 resetRouter.post(
   "/:token",
-  [
-    check(
-      "password",
-      "Password must contain at least 8 signs and maximum 24 signs, include uppercase and lowercase letters, a number and a special character"
-    ).matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/),
-  ],
+  check(
+    "password",
+    "Password must contain at least 8 signs and maximum 24 signs, include uppercase and lowercase letters, a number and a special character"
+  ).matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/),
   controller.resetPassword
 );
