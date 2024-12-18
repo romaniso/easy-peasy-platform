f9abbebee873dc9b3eceb673bb0a3bb30ad082b7
Roman Isopenko <isopenkoroman@gmail.com>
2024-12-18 22:27:58 +0100

    feat: add download btn and handleDownload helper function in Image

--- client/src/components/common/Image.tsx
+++ client/src/components/common/Image.tsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from "react";
-import { MdZoomOutMap, MdZoomInMap } from "react-icons/md";
+import { useState } from "react";
+import { MdZoomOutMap } from "react-icons/md";
+import { FaCloudDownloadAlt } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+
 import Modal from "./Modal";
 
 interface Props {
@@ -14,9 +17,25 @@ export const Image = ({ src, alt, ...props }: Props) => {
     setZoomedIn(!isZoomedIn);
   };
 
-  useEffect(() => {
-    console.log(isZoomedIn);
-  }, [isZoomedIn]);
+  const handleDownload = async () => {
+    // console.log(`Downloading file ${src}...`);
+    // const imageUrl = src; // Replace with your dynamic URL
+    // try {
+    //   const res = await fetch(imageUrl);
+    //   const blob = await res.blob();
+    //   const url = window.URL.createObjectURL(blob);
+    //   const a = document.createElement("a");
+    //   a.style.display = "none";
+    //   a.href = url;
+    //   a.download = "dynamic-image.jpg"; // Name for the downloaded file
+    //   document.body.appendChild(a);
+    //   a.click();
+    //   window.URL.revokeObjectURL(url);
+    //   document.body.removeChild(a);
+    // } catch (err) {
+    //   console.error("Error downloading the image", err);
+    // }
+  };
 
   return (
     <>
@@ -34,11 +53,18 @@ export const Image = ({ src, alt, ...props }: Props) => {
       {isZoomedIn && (
         <Modal onClose={() => setZoomedIn(false)} size="md:w-2/3 md:h-3/4">
           <div className="w-full h-full grid place-items-center md:py-2 md:px-4 relative">
-            <MdZoomInMap
-              className="text-4xl absolute bottom-7 right-7 p-1 bg-indigo-500 dark:bg-white/20 rounded-md hover:scale-110 cursor-pointer transition-transform text-white dark:text-orange-500 shadow-md"
+            <RxCross2
+              className="text-4xl absolute top-2 right-2 p-1 bg-indigo-500 dark:bg-white/20 rounded-md hover:scale-110 cursor-pointer transition-transform text-white dark:text-orange-500 shadow-md"
               onClick={handleZoom}
             />
             <img src={src} alt={alt} className="w-full" {...props} />
+            <div className="absolute bottom-2 right-2 flex items-center gap-3">
+              {/* Zoom in and out - 100% + */}
+              <FaCloudDownloadAlt
+                className="text-4xl p-1 bg-indigo-500 dark:bg-white/20 rounded-md hover:scale-110 cursor-pointer transition-transform text-white dark:text-orange-500 shadow-md"
+                onClick={handleDownload}
+              />
+            </div>
           </div>
         </Modal>
       )}
