bee62f737fb617f3e2fe30be3f0040564741555c
Roman Isopenko <109075222+RomanIsopenko@users.noreply.github.com>
2025-02-24 08:14:09 +0100

    feat: add testing version

--- client/src/hooks/useIntersectionObserver.tsx
+++ client/src/hooks/useIntersectionObserver.tsx
@@ -1,13 +1,21 @@
-import { useEffect, useRef, useState } from "react";
-import { Article } from "../interfaces/article";
+import { useEffect, useState, useRef } from "react";
 
-export function useIntersectionObserver(data: Article) {
-  const observer = useRef<IntersectionObserver | null>(null);
+export function useIntersectionObserver() {
   const [activeId, setActiveId] = useState("");
+  const observerRef = useRef<IntersectionObserver | null>(null);
+  //  const hasInsertedDebugElements = useRef(false); // Prevents duplicate divs
 
   useEffect(() => {
+    const initialActiveId = document.querySelector<HTMLHeadElement>(
+      ".markdown-content h2"
+    );
+    if (initialActiveId) {
+      setActiveId(initialActiveId.id);
+    }
+
     const handleObserver: IntersectionObserverCallback = (entries) => {
       entries.forEach((entry) => {
+        console.log(entry);
         if (entry.isIntersecting) {
           console.log("Active entry:", entry);
           setActiveId(entry.target.id);
@@ -15,26 +23,44 @@ export function useIntersectionObserver(data: Article) {
       });
     };
 
-    observer.current = new IntersectionObserver(handleObserver, {
-      threshold: 1,
-      rootMargin: "-30% 0px -40% 0px",
+    observerRef.current = new IntersectionObserver(handleObserver, {
+      threshold: 0.5,
+      rootMargin: "-30% 0px -60% 0px",
     });
 
-    const elements: NodeListOf<HTMLHeadElement> = document.querySelectorAll(
+    // if (!hasInsertedDebugElements.current) {
+    //   hasInsertedDebugElements.current = true;
+    //   const start = document.createElement("div");
+    //   start.style.border = "solid green 3px";
+    //   start.style.position = "fixed";
+    //   start.style.top = "30%";
+    //   start.style.right = "0";
+    //   start.style.width = "90%";
+    //   const end = document.createElement("div");
+    //   end.style.border = "solid red 3px";
+    //   end.style.position = "fixed";
+    //   end.style.bottom = "60%";
+    //   end.style.right = "0";
+    //   end.style.width = "90%";
+    //   document.body.append(start, end);
+    // }
+
+    const elements = document.querySelectorAll<HTMLHeadElement>(
       ".markdown-content h2, .markdown-content h3"
     );
 
     if (elements.length > 0) {
       console.log("Observing elements:", elements);
-      elements.forEach((elem) => observer.current?.observe(elem));
+      elements.forEach((elem) => observerRef.current?.observe(elem));
     } else {
       console.warn("No headings found to observe. Retrying...");
     }
+
     return () => {
-      observer.current?.disconnect();
+      observerRef.current?.disconnect();
       console.log("Observer disconnected.");
     };
-  }, [data]);
+  }, [activeId]);
 
   return { activeId };
 }
