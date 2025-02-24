34056af0f0de1540210b9be392e3652727aed386
Roman Isopenko <109075222+RomanIsopenko@users.noreply.github.com>
2025-02-24 08:15:08 +0100

    fix: remove comments

--- client/src/hooks/useIntersectionObserver.tsx
+++ client/src/hooks/useIntersectionObserver.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState, useRef } from "react";
 export function useIntersectionObserver() {
   const [activeId, setActiveId] = useState("");
   const observerRef = useRef<IntersectionObserver | null>(null);
-  //  const hasInsertedDebugElements = useRef(false); // Prevents duplicate divs
 
   useEffect(() => {
     const initialActiveId = document.querySelector<HTMLHeadElement>(
@@ -27,23 +26,6 @@ export function useIntersectionObserver() {
       threshold: 0.5,
       rootMargin: "-30% 0px -60% 0px",
     });
-
-    // if (!hasInsertedDebugElements.current) {
-    //   hasInsertedDebugElements.current = true;
-    //   const start = document.createElement("div");
-    //   start.style.border = "solid green 3px";
-    //   start.style.position = "fixed";
-    //   start.style.top = "30%";
-    //   start.style.right = "0";
-    //   start.style.width = "90%";
-    //   const end = document.createElement("div");
-    //   end.style.border = "solid red 3px";
-    //   end.style.position = "fixed";
-    //   end.style.bottom = "60%";
-    //   end.style.right = "0";
-    //   end.style.width = "90%";
-    //   document.body.append(start, end);
-    // }
 
     const elements = document.querySelectorAll<HTMLHeadElement>(
       ".markdown-content h2, .markdown-content h3"
