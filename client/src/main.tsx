7d65098f20b86177c7db7b8373f7e90e56609d3e
Roman Isopenko <109075222+RomanIsopenko@users.noreply.github.com>
2025-03-03 10:08:48 +0100

    feat: connect react with redux using Provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,22 +8,25 @@ import { AuthProvider } from "./context/AuthContext";
 import { UserProvider } from "./context/UserContext";
 import "./services/i18n";
 import { Loader } from "./components/common/Loader";
+import { Provider } from "react-redux";
+import { store } from "./state/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* @TODO: loading component */}
-    <React.Suspense fallback={<Loader />}>
-      <Router>
-        <AuthProvider>
-          <UserProvider>
-            <ToastContextProvider>
-              <Routes>
-                <Route path="/*" element={<App />} />
-              </Routes>
-            </ToastContextProvider>
-          </UserProvider>
-        </AuthProvider>
-      </Router>
-    </React.Suspense>
+    <Provider store={store}>
+      <React.Suspense fallback={<Loader />}>
+        <Router>
+          <AuthProvider>
+            <UserProvider>
+              <ToastContextProvider>
+                <Routes>
+                  <Route path="/*" element={<App />} />
+                </Routes>
+              </ToastContextProvider>
+            </UserProvider>
+          </AuthProvider>
+        </Router>
+      </React.Suspense>
+    </Provider>
   </React.StrictMode>
 );
