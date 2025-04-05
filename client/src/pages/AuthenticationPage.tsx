018f84f65233c74a29cfc0d07e67eaca5495b089
romaniso <isopenkoroman@gmail.com>
2025-04-05 22:54:24 +0200

    refactor: integrate accessToken check in AuthenticationPage to redirect authenticated users

--- client/src/pages/AuthenticationPage.tsx
+++ client/src/pages/AuthenticationPage.tsx
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTop } from "../hooks/useTop";
 import { Login } from "../components/auth/Login";
 import { Register } from "../components/auth/Register";
-//import BgImage from "../assets/images/bg-auth.jpg";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+import { useNavigate } from "react-router-dom";
 
 export const AuthenticationPage = (): JSX.Element => {
   useTop();
   const [toggle, setToggle] = useState(true);
+  const { accessToken } = useSelector((state: RootState) => state.user.user);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (accessToken) {
+      navigate("/dashboard");
+    }
+  }, [accessToken, navigate]);
 
   const handleToggle = () => {
     setToggle(!toggle);
   };
   return (
     <section className="min-h-screen flex items-center justify-center">
-      {/*<img
-        src={BgImage}
-        alt="background image"
-        className="absolute inset-0 w-full object-fill opacity-10 dark:opacity-5"
-      />*/}
       {toggle ? (
         <Login onToggleForm={handleToggle} />
       ) : (
