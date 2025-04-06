06a7b52a21fd89d337a1c501343b7beba9254109
romaniso <isopenkoroman@gmail.com>
2025-04-06 10:46:04 +0200

    fix: add optional chaining for user properties in ProfilePage component

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -15,7 +15,7 @@ export const ProfilePage = (): JSX.Element => {
         <div className="flex-shrink h-1/5 md:h-full md:basis-2/5 flex flex-col justify-center items-center bg-gradient-to-r from-orange-500 to-orange-300 dark:from-stone-900 dark:to-stone-800 p-3 md:p-5">
           <ProfileAvatar />
           <p className="text-center mt-1 md:mt-10 text-xl md:text-3xl text-indigo-800 dark:text-indigo-300 font-extrabold drop-shadow">
-            {user.profile.firstName || user.username} {user.profile.lastName}
+            {user?.profile.firstName || user?.username} {user?.profile.lastName}
           </p>
           <blockquote className="text-center mt-10 text-lg text-indigo-900 dark:text-indigo-200 border-t border-indigo-600/50 dark:border-indigo-200/50 pt-8 italic md:block hidden">
             {t("proverb.content")}
